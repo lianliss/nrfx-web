704e86b2295bca9f170030b5527e193580a870f8
goglev <goglev-andrey@mail.ru>
2019-06-07 19:01:39 +0300

    working on homepage..

--- src/wrappers/Site/SiteWrapper.js
+++ src/wrappers/Site/SiteWrapper.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './SiteWrapper.less';
 import Header from '../../components/site/Header/Header';
 import Footer from '../../components/site/Footer/Footer';
+import SVG from 'react-inlinesvg';
 
 export default function SiteWrapper(props) {
   return (
     <div>
       <div className="SiteWrapper">
+        <div className="SiteWrapper__bg">
+          <SVG src={require('../../asset/site/head_bg.svg')} />
+        </div>
         <Header />
         {props.children}
       </div>
