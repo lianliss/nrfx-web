ed0a008610c64b50d6e3d27e623fd9ee4626b909
goglev <goglev-andrey@mail.ru>
2019-06-28 23:05:07 +0300

    fix langs and redirect to cabinet after login

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,9 +5,9 @@ import { TranslationApi } from '../swagger';
 import callApi from '../services/api';
 import store from '../store';
 
-export function loadLang() {
+export function loadLang(lang) {
   return new Promise((resolve, reject) => {
-    callApi(new TranslationApi().translationExportGet, 'ru').then((lang) => {
+    callApi(new TranslationApi().translationExportGet, lang).then((lang) => {
       store.dispatch({type: actionTypes.SET_LANG, lang});
       resolve();
     }).catch((err) => reject(err));
