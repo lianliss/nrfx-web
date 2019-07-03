1301aeab95d4b49be4f693454479efe1dc2dacdd
elinahovakimyan <elinahovakimyan@gmail.com>
2019-07-03 15:57:25 +0400

    Add social links

--- src/components/site/Footer/Footer.js
+++ src/components/site/Footer/Footer.js
@@ -13,22 +13,22 @@ export default function Footer() {
         <div className="Footer__links_wrap">
           <div className="Footer__links">
             <div className="Footer__links__title">{utils.getLang('site__footerProducts')}</div>
-            <a href={`/#/${pages.WALLET}`} className="Footer__links__item">{utils.getLang('site__footerWallet')}</a>
-            <a href={`/#/${pages.EXCHANGE}`} className="Footer__links__item">{utils.getLang('site__footerExchange')}</a>
-            <a href={`/#/${pages.ROBOTS}`} className="Footer__links__item">{utils.getLang('site__footerRobots')}</a>
-            <a href={`/#/${pages.INVESTMENT}`} className="Footer__links__item">{utils.getLang('site__footerInvestments')}</a>
-            <a href={`/#/${pages.COMMERCE}`} className="Footer__links__item">{utils.getLang('site__footerPayment')}</a>
+            <a href={`/${pages.WALLET}`} className="Footer__links__item">{utils.getLang('site__footerWallet')}</a>
+            <a href={`/${pages.EXCHANGE}`} className="Footer__links__item">{utils.getLang('site__footerExchange')}</a>
+            <a href={`/${pages.ROBOTS}`} className="Footer__links__item">{utils.getLang('site__footerRobots')}</a>
+            <a href={`/${pages.INVESTMENT}`} className="Footer__links__item">{utils.getLang('site__footerInvestments')}</a>
+            <a href={`/${pages.COMMERCE}`} className="Footer__links__item">{utils.getLang('site__footerPayment')}</a>
           </div>
           <div className="Footer__links">
             <div className="Footer__links__title">{utils.getLang('site__footerCompany')}</div>
-            <a href={`/#/${pages.ABOUT}`} className="Footer__links__item">{utils.getLang('site__footerAboutUs')}</a>
-            <a href={`/#/${pages.TECHNOLOGY}`} className="Footer__links__item">{utils.getLang('site__footerTechnology')}</a>
-            <a href={`/#/${pages.SAFETY}`} className="Footer__links__item">{utils.getLang('site__footerSecurity')}</a>
+            <a href={`/${pages.ABOUT}`} className="Footer__links__item">{utils.getLang('site__footerAboutUs')}</a>
+            <a href={`/${pages.TECHNOLOGY}`} className="Footer__links__item">{utils.getLang('site__footerTechnology')}</a>
+            <a href={`/${pages.SAFETY}`} className="Footer__links__item">{utils.getLang('site__footerSecurity')}</a>
           </div>
           <div className="Footer__links">
             <div className="Footer__links__title">{utils.getLang('site__footerHelp')}</div>
-            <a href={`/#/${pages.FAQ}`} className="Footer__links__item">{utils.getLang('site__footerFAQ')}</a>
-            <a href={`/#/${pages.CONTACT}`} className="Footer__links__item">{utils.getLang('site__footerContactUs')}</a>
+            <a href={`/${pages.FAQ}`} className="Footer__links__item">{utils.getLang('site__footerFAQ')}</a>
+            <a href={`/${pages.CONTACT}`} className="Footer__links__item">{utils.getLang('site__footerContactUs')}</a>
             <a href="#" className="Footer__links__item">{utils.getLang('site__footerTermsUse')}</a>
             <a href="#" className="Footer__links__item">{utils.getLang('site__footerPrivacyPolicy')}</a>
           </div>
@@ -44,16 +44,16 @@ export default function Footer() {
           </div>
           <div className="Footer__copyright">© 2017-2019 BITCOINBOT</div>
           <div className="Footer__socials">
-            <a href="#" className="Footer__social">
+            <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/Bitcoinbotpro-432506870546401/" className="Footer__social">
               <SVG src={require('../../../asset/site/footer_facebook.svg')} />
             </a>
-            <a href="#" className="Footer__social">
+            <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/BitcoinBot_pro" className="Footer__social">
               <SVG src={require('../../../asset/site/footer_twitter.svg')} />
             </a>
-            <a href="#" className="Footer__social">
+            <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/bitcoinbot_pro/" className="Footer__social">
               <SVG src={require('../../../asset/site/footer_instagram.svg')} />
             </a>
-            <a href="#" className="Footer__social">
+            <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UCkwFUCZP4B9sghoTFJFj2cA" className="Footer__social">
               <SVG src={require('../../../asset/site/footer_youtube.svg')} />
             </a>
           </div>
