260bfbaf3b9f4c50b679cf83384af8636aab07ac
goglev <goglev-andrey@mail.ru>
2019-07-08 14:11:03 +0300

    update loadLang response format

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,8 +7,8 @@ import store from '../store';
 
 export function loadLang(lang) {
   return new Promise((resolve, reject) => {
-    callApi(new TranslationApi().translationExportGet, lang).then((lang) => {
-      store.dispatch({type: actionTypes.SET_LANG, lang});
+    callApi(new TranslationApi().translationExportGet, lang).then(({ translations, languages }) => {
+      store.dispatch({type: actionTypes.SET_LANG, lang: translations});
       resolve();
     }).catch((err) => reject(err));
   });
