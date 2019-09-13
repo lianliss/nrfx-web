9f4a5ba128c1f59ee0cd38816cc8b694acfbf2b5
Denis Borodin <borodindk@gmail.com>
2019-09-13 13:54:48 +0300

    work on OpenDepositModal

--- src/components/cabinet/OpenDepositModal/OpenDepositModal.js
+++ src/components/cabinet/OpenDepositModal/OpenDepositModal.js
@@ -3,11 +3,103 @@ import './OpenDepositModal.less';
 import React from 'react';
 import UI from '../../../ui';
 import SVG from 'react-inlinesvg';
+import apiSchema from '../../../services/apiSchema';
+import * as api from "../../../services/api";
+import * as walletsActions from "../../../actions/cabinet/wallets";
+import * as actions from "../../../actions";
+import * as utils from "../../../utils";
+import { connect } from "react-redux";
 
-export default class OpenDepositModal extends React.Component {
+class OpenDepositModal extends React.Component {
   state = {
-    selectDepositType: 'static'
+    walletCurrentOption: {},
+    walletOptions: [],
+    selectDepositType: 'static',
+    planOptions: [],
+    planCurrentOption: {},
+    amountMax: 0,
+    amountMin: 0,
+    currency: this.props.router.route.params.currency.toLowerCase(),
+    touched: false
   };
+
+  componentDidMount() {
+    this.__getWalets();
+  }
+
+  __getWalets() {
+    walletsActions.getWallets().then((wallets) => {
+      wallets = wallets.filter(w => w.status !== "pending");
+      const walletOptions = wallets.map((item) => {
+        const info = actions.getCurrencyInfo(item.currency);
+        return {
+          title: utils.ucfirst(info.name),
+          note: `${utils.formatDouble(item.amount)} ${item.currency.toUpperCase()}`,
+          value: item.id,
+          currency: item.currency
+        }
+      });
+      const walletCurrentOption =  walletOptions.find(w => w.currency === this.state.currency );
+      this.setState({
+        walletOptions,
+        walletCurrentOption,
+        currency: walletCurrentOption.currency,
+        walletId: walletCurrentOption.value
+      }, this.__getPlans);
+    }).catch(() => {
+      alert("Error");
+    });
+  }
+
+  __getPlans() {
+    const { currency, amount, selectDepositType } = this.state;
+
+    currency && selectDepositType &&
+    api.call(apiSchema.Investment.PlansGet, {
+      currency,
+      amount: amount || 0,
+      deposit_type: selectDepositType
+    }, ).then(({plans}) => {
+
+      const planOptions = plans.map(p => ({
+        value: p.id,
+        title: p.description,
+        note: `${p.percent}% ${p.days} Days`,
+        max: p.max,
+        min: p.min
+      }));
+
+      let planCurrentOption = planOptions.find(p => p.value === this.state.planId) || planOptions[0];
+
+      this.setState({
+        planOptions,
+        planCurrentOption,
+        amountMax: planCurrentOption.max,
+        amountMin: planCurrentOption.min,
+      });
+    }).catch((err) => {
+      this.setState({error: err.message});
+    });
+  }
+
+  handleSubmit() {
+    !this.state.touched && this.setState({ touched: true });
+
+    this.state.amount && api.call(apiSchema.Investment.DepositPut, {
+      amount: this.state.amount,
+      wallet_id: this.state.walletId,
+      plan_id: this.state.planId,
+      deposit_type: this.state.selectDepositType
+    }).then(({plans}) => {
+      this.setState({ plans }, () => {
+
+      });
+    }).catch((err) => {
+      this.setState({error: err.message});
+    });
+  }
+
+  __getPlansThrottle = utils.throttle(this.__getPlans, 1000);
 
   render() {
     let typeInfoRows;
@@ -47,21 +139,36 @@ export default class OpenDepositModal extends React.Component {
         <div className="OpenDepositModal">
           <div className="OpenDepositModal__row">
             <UI.Dropdown
-              placeholder={{ title: 'Bitcoin Profit', note: '20 BTC' }}
-              options={[]}
-              onChange={() => console.log('changed')}
+              placeholder={this.state.walletCurrentOption}
+              options={this.state.walletOptions}
+              onChange={item => {
+                item && this.setState({
+                  walletCurrentOption: item,
+                  currency: item.currency,
+                  walletId: item.value
+                }, this.__getPlansThrottle)
+              }}
             />
           </div>
           <div className="OpenDepositModal__row">
             <UI.Input
+              error={
+                !this.state.amount && this.state.touched ||
+                this.state.amount > this.state.amountMax ||
+                this.state.amount < this.state.amountMin
+              }
+              value={this.state.amount}
               placeholder="Amount"
-              indicator="min 0.04 BTC"
+              indicator={`min ${this.state.amountMin} ${this.state.currency.toUpperCase()}`}
+              onTextChange={amount => {
+                this.setState({amount: (amount || "").replace(/\D+/g,"")}, this.__getPlansThrottle);
+              }}
             />
           </div>
           <div className="OpenDepositModal__row">
             <UI.SwitchTabs
               selected={this.state.selectDepositType}
-              onChange={(selectDepositType) => this.setState({ selectDepositType })}
+              onChange={(selectDepositType) => this.setState({ selectDepositType }, this.__getPlans)}
               tabs={[
                 { value: 'static', label: 'Static' },
                 { value: 'dynamic', label: 'Dynamic' }
@@ -81,16 +188,28 @@ export default class OpenDepositModal extends React.Component {
           </div>
           <div className="OpenDepositModal__row">
             <UI.Dropdown
-              placeholder={{ title: 'Optimal', note: '153% 365 Days' }}
-              options={[]}
-              onChange={() => console.log('changed')}
+              placeholder={this.state.planCurrentOption}
+              options={this.state.planOptions}
+              onChange={item => {
+                item && this.setState({
+                  planId: item.value,
+                  planCurrentOption: item,
+                  amountMax: item.max,
+                  amountMin: item.min
+                }, this.__getPlansThrottle)
+              }}
             />
           </div>
+          { this.state.error && <div className="OpenDepositModal__error">{this.state.error}</div>}
           <div className="OpenDepositModal__btn_wrapper">
-            <UI.Button>Invest</UI.Button>
+            <UI.Button onClick={this.handleSubmit.bind(this)}>Invest</UI.Button>
           </div>
         </div>
       </UI.Modal>
     )
   }
 }
+
+export default connect(state => ({
+  router: state.router
+}))(OpenDepositModal);
--- src/utils/index.js
+++ src/utils/index.js
@@ -74,6 +74,29 @@ export const formatNumber = (num, minimumFractionDigits = 2, maximumFractionDigi
   return null;
 };
 
+export function throttle(func, ms) {
+  let isThrottled = false,
+    savedArgs,
+    savedThis;
+  function wrapper() {
+    if (isThrottled) {
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+    func.apply(this, arguments);
+    isThrottled = true;
+    setTimeout(function() {
+      isThrottled = false;
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+  return wrapper;
+}
+
 export function ucfirst(input) {
   return input.charAt(0).toUpperCase() + input.slice(1);
 }
