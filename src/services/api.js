d445287d38c4aa87951fc16d4a6182ff3a92f4e0
Denis Borodin <borodindk@gmail.com>
2019-09-16 23:44:09 +0300

    fix api url again

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import * as auth from './auth';
 
-const API_ENTRY = 'https://stage.bitcoinbot.pro';
+const API_ENTRY = 'https://api.bitcoinbot.pro';
 const API_VERSION = 1;
 
 export const EXPORT_API_VERSION = API_VERSION;
