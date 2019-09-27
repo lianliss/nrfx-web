7c998b6b958759bf7c077fe6f065aee8a21b34b2
Denis Borodin <borodindk@gmail.com>
2019-09-27 17:58:40 +0300

    fix WithdrawalModal

--- src/components/cabinet/WithdrawalModal/WithdrawalModal.js
+++ src/components/cabinet/WithdrawalModal/WithdrawalModal.js
@@ -79,7 +79,7 @@ class WithdrawalModal extends React.Component {
                   value={this.state.amount}
                   error={this.state.amount > payment.available}
                 />
-                <p className="Form__helper__text">{utils.getLang("global_available")}: {payment.available} {currency}</p>
+                <p className="Form__helper__text">{utils.getLang("global_available")}: {utils.formatDouble(payment.available)} {currency}</p>
               </div>
               <UI.Button
                 currency={currencyInfo.abbr}
