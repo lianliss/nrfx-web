ffa03575c12731ad8ca7f8d24a6ef4a5b857c314
Denis Borodin <borodindk@gmail.com>
2019-10-14 19:02:03 +0300

    fix plan percen

--- src/components/cabinet/DepositInfoModal/DepositInfoModal.js
+++ src/components/cabinet/DepositInfoModal/DepositInfoModal.js
@@ -41,7 +41,7 @@ export default class DepositInfoModal extends React.Component {
               <InfoRow label={utils.getLang("global_amount")}>{deposit.amount} {currency}</InfoRow>
               <InfoRow label={utils.getLang("cabinet_investmentsScreen_profit")}>{utils.formatDouble(deposit.profit, 8)} {currency} ({utils.formatDouble(deposit.current_percent, 2)}%)</InfoRow>
               <InfoRow label={utils.getLang("in_fiat")}>{utils.formatDouble(deposit.usd_profit, 2)} USD</InfoRow>
-              <InfoRow label={utils.getLang("global_estimated")}>{utils.formatDouble(deposit.percent, 2)} %</InfoRow>
+              <InfoRow label={utils.getLang("global_estimated")}>{utils.formatDouble(deposit.percent, 2)}%</InfoRow>
             </InfoRowGroup>
           </div>
           {/*<div className="DepositInfoModal__withdrawal_form" style={{display:'flex'}}>*/}
