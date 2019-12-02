26cf24fe4c1d3b5ccc7605f6b82c56a74553ce69
Denis Borodin <borodindk@gmail.com>
2019-12-02 15:56:29 +0800

    fix pool lang

--- src/index/components/cabinet/OpenDepositModal/OpenDepositModal.js
+++ src/index/components/cabinet/OpenDepositModal/OpenDepositModal.js
@@ -187,17 +187,13 @@ class OpenDepositModal extends React.Component {
       ],
       pool: [
         {
-          label: utils.getLang('cabinet_openNewDeposit_income'),
+          label: utils.getLang('cabinet_openNewDeposit_pool_income'),
           icon: require('../../../../asset/24px/bar-chart.svg')
         },
         {
           label: utils.getLang('cabinet_openNewDeposit_special'),
           icon: require('../../../../asset/24px/star.svg')
         },
-        {
-          label: utils.getLang('cabinet_openNewDeposit_conclusion'),
-          icon: require('../../../../asset/24px/withdraw.svg')
-        }
       ],
     };
 
