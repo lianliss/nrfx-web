98833fc6d15774477c3ab51d67aef27008cfee99
Denis Borodin <borodindk@gmail.com>
2020-02-08 14:30:04 +0800

    FE-108: update currencies after login

--- src/actions/user.js
+++ src/actions/user.js
@@ -7,6 +7,7 @@ import * as actionTypes from './actionTypes';
 import * as api from '../services/api';
 import * as auth from '../services/auth';
 import * as internalNotifications from './cabinet/internalNotifications';
+import * as actions from './index';
 
 export function install() {
   if (!auth.isLogged()) {
@@ -14,6 +15,7 @@ export function install() {
   }
   return api.call(apiSchema.Profile.DefaultGet).then(({ ...props }) => {
     store.dispatch({ type: actionTypes.PROFILE, props });
+    actions.loadCurrencies();
     internalNotifications.load()(store.dispatch, store.getState);
   });
 }
