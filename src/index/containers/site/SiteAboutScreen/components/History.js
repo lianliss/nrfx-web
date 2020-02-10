8376418d28edf93b5ed21437ad2ca18e0eed5db4
Denis Borodin <borodindk@gmail.com>
2020-02-10 19:00:29 +0800

    fix history

--- src/index/containers/site/SiteAboutScreen/components/History.js
+++ src/index/containers/site/SiteAboutScreen/components/History.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import TitleWithBg from '../../../../components/site/TitleWithBg/TitleWithBg';
 import InfoSection from '../../../../components/site/InfoSection/InfoSection';
 import Timeline from '../../../../components/site/Timeline/Timeline';
+import { getLang } from 'src/utils/index';
 import { data } from '../fixtures';
 
 
@@ -11,8 +12,8 @@ function History({ lang }) {
   return (
     <>
       <div className="SiteAboutScreen__intro SiteAboutScreen__history">
-        <TitleWithBg title={lang.site__aboutHistoryTitle} bgTitle={lang.site__aboutHistoryTitle} centered />
-        <p className="SiteAboutScreen__caption">{lang.site__aboutHistorySubTitle}</p>
+        <TitleWithBg title={getLang('site__aboutHistoryTitle')} bgTitle={getLang('site__aboutHistoryTitle')} centered />
+        <p className="SiteAboutScreen__caption">{getLang('site__aboutHistoryTitle')}</p>
       </div>
 
       <div className="SiteAboutScreen__history__timeline">
