59f33abf448d8319c43a2f3849d4b4d43afbd1b1
Denis Borodin <borodindk@gmail.com>
2020-02-10 14:01:00 +0800

    deprecated isExchangeEnabled

--- src/index/components/cabinet/Header/AdaptiveHeader.js
+++ src/index/components/cabinet/Header/AdaptiveHeader.js
@@ -60,7 +60,6 @@ AdaptiveHeader.defaultProps = {
   rightContent: ""
 };
 
-
 export default connect(state => ({
   internalNotifications: state.internalNotifications,
   profile: state.default.profile,
@@ -68,7 +67,6 @@ export default connect(state => ({
   router: state.router,
   langList: state.default.langList,
   title: state.default.title,
-  isExchangeEnabled: state.default.profile.is_exchange_enabled
 }), {
   dropInternalNotifications: internalNotifications.drop,
   loadNotifications: notificationsActions.loadNotifications,
