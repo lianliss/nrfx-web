8e6385e116f2e71188b6e9c046e40111369ee31c
Denis Borodin <borodindk@gmail.com>
2020-02-24 15:25:01 +0800

    set api url

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,8 @@ import { clearProfile } from '../actions/auth';
 import router from '../router';
 import * as adminPages from '../admin/constants/pages';
 
-export const API_ENTRY = 'https://api.narfex.com';
-// export const API_ENTRY = 'https://stageapi.bitcoinbot.pro';
+// export const API_ENTRY = 'https://api.narfex.com';
+export const API_ENTRY = 'https://api-payments.bitcoinbot.pro';
 export const API_VERSION = 1;
 
 export const Errors = {
