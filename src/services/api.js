89aff0fe1c624886b2e1f8b64a91f22e0189f366
Denis Borodin <borodindk@gmail.com>
2020-03-12 17:24:07 +0800

    fix api

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,8 @@ import { clearProfile } from '../actions/auth';
 import router from '../router';
 import * as adminPages from '../admin/constants/pages';
 
-// export const API_ENTRY = 'https://api.narfex.com';
-export const API_ENTRY = 'https://api-admin.bitcoinbot.pro';
+export const API_ENTRY = 'https://api.narfex.com';
+// export const API_ENTRY = 'https://api-stage.bitcoinbot.pro';
 export const API_VERSION = 1;
 
 export function invoke(method, name, params, options = {}) {
