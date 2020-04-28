b3d882a9ba14e18e7dbd1bf22826928f3adab05f
Denis Borodin <borodindk@gmail.com>
2020-04-28 22:09:27 +0800

    FE-249: Fix admin tabs & other layout

--- config/branches.js
+++ config/branches.js
@@ -1,6 +1,8 @@
 module.exports = {
   // front-end: back-end branch
   'fe-239': 'xendit',
+  'fe-249': 'infouseradmin',
   'fe-242': 'documentation',
   'fe-243': 'withdrawal',
+  'fe-251': 'agents',
 };
