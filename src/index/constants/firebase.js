2ffe7d904c5292deae3165564aa41a3e5173de3d
Denis Borodin <borodindk@gmail.com>
2020-08-04 18:30:39 +0800

    fix firebase

---
+++ src/index/constants/firebase.js
@@ -0,0 +1,10 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD--i-HdRJeH5nk1c_D_LTPwkrhBU5cz4Y",
+  authDomain: "narfex-com.firebaseapp.com",
+  databaseURL: "https://narfex-com.firebaseio.com",
+  projectId: "narfex-com",
+  storageBucket: "narfex-com.appspot.com",
+  messagingSenderId: "487369773798",
+  appId: "1:487369773798:web:353afafd236b5ac22ee127",
+  measurementId: "G-WMDH695XP3"
+};
