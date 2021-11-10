2c96673af8f04694aaad94cfa69bda5f3a26f820
Alex <alex.sokolov@live.com>
2021-11-10 14:47:01 +0100

    Add lines in RoadMap

--- src/index/containers/site/SiteTokenScreen/SiteTokenScreen.js
+++ src/index/containers/site/SiteTokenScreen/SiteTokenScreen.js
@@ -30,20 +30,21 @@ const SiteTokenScreen = props => {
   };
 
   const roadMap = [
-    { title: getLang("token_roadMapStep1"), phase: getLang("token_roadpMapPhase1") },
-    { title: getLang("token_roadMapStep2"), phase: getLang("token_roadpMapPhase1") },
-    { title: getLang("token_roadMapStep3"), phase: getLang("token_roadpMapPhase1") },
+    { title: getLang("token_roadMapStep1"), phase: getLang("token_roadMapPhase1") },
+    { title: getLang("token_roadMapStep2"), phase: getLang("token_roadMapPhase1") },
+    { title: getLang("token_roadMapStep3"), phase: getLang("token_roadMapPhase1") },
     { title: getLang("token_roadMapStep4"), phase: getLang("token_roadMapPhase1") },
     { title: getLang("token_roadMapStep5"), phase: getLang("token_roadMapPhase1") },
     { title: getLang("token_roadMapStep6"), phase: getLang("token_roadMapPhase1") },
     { title: getLang("token_roadMapStep7"), phase: getLang("token_roadMapPhase1") },
     { title: getLang("token_roadMapStep8"), phase: getLang("token_roadMapPhase1") },
-    { title: getLang("token_roadMapStep9"), phase: getLang("token_roadMapPhase2") },
+    { title: getLang("token_roadMapStep9"), phase: getLang("token_roadMapPhase1") },
     { title: getLang("token_roadMapStep10"), phase: getLang("token_roadMapPhase2") },
     { title: getLang("token_roadMapStep11"), phase: getLang("token_roadMapPhase2") },
     { title: getLang("token_roadMapStep12"), phase: getLang("token_roadMapPhase2") },
     { title: getLang("token_roadMapStep13"), phase: getLang("token_roadMapPhase2") },
-    { title: getLang("token_roadMapStep14"), phase: getLang("token_roadMapPhase2") }
+    { title: getLang("token_roadMapStep14"), phase: getLang("token_roadMapPhase2") },
+    { title: getLang("token_roadMapStep15"), phase: getLang("token_roadMapPhase2") }
   ];
 
   return (
