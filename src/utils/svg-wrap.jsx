71adc79f31209f603da1a3081cd7339806df17cd
Danil Sakhinov <ldevil2209@ya.ru>
2021-12-20 21:11:15 +0800

    Fix wrong svg display

--- src/utils/svg-wrap.jsx
+++ src/utils/svg-wrap.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import InlineSVG from "react-inlinesvg";
 
 export default props => {
   const {src} = props;
-  return <InlineSVG src={src.default || src} />
+  const [isLoaded, setIsLoaded] = useState(false);
+  return <span className={`isvg ${isLoaded ? 'loaded' : 'loading'}`}>
+    <InlineSVG
+      {...props}
+      onLoad={() => setIsLoaded(true)}
+      onError={error => console.error('[SVG]', src.default || src, props)}
+      uniquifyIDs={true}
+      src={src.default || src} />
+  </span>
 };
