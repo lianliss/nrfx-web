cf6293a67f8658504e1e44c9549552be457d5799
Danil Sakhinov <ldevil2209@ya.ru>
2021-12-25 23:59:55 +0800

    Fix WatchButton bg import

--- src/ui/components/Button/WatchButton.js
+++ src/ui/components/Button/WatchButton.js
@@ -10,7 +10,9 @@ function WatchButton(props) {
       onClick={() => props.onClick && props.onClick()}
       style={props.style}
     >
-      <div className="WatchButton__icon" />
+      <div className="WatchButton__icon" style={{
+        backgroundImage: `url(${require("../../asset/watch_button_icon_28.svg").default})`
+      }} />
       <div className="WatchButton__label">{props.children}</div>
     </div>
   );
