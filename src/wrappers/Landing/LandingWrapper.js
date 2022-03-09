a46c14deead82672f9e01867752e7bf0543f0193
Danil Sakhinov <ldevil2209@ya.ru>
2022-03-09 19:49:34 +0800

    Add Google Tag Manager

--- src/wrappers/Landing/LandingWrapper.js
+++ src/wrappers/Landing/LandingWrapper.js
@@ -4,11 +4,15 @@ import Ticker from "../../landing/components/Ticker/Ticker";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Notice from "./components/Notice/Notice";
+import TagManager from 'react-gtm-module';
 
 import { useRoute } from "react-router5";
 
 export default props => {
   const route = useRoute();
+  TagManager.initialize({
+    gtmId: 'GTM-NSSCKZG',
+  });
 
   useEffect(() => {
     window.scroll(0, 0);
