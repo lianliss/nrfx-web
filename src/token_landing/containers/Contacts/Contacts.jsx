88b258a1fd6fcabfe1c0a74221fa5d8e967d64e7
Arslan-95 <arslan180198@gmail.com>
2022-03-30 16:58:13 +0300

    [NARFEX-3] Contacts block - is ready

--- src/token_landing/containers/Contacts/Contacts.jsx
+++ src/token_landing/containers/Contacts/Contacts.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import './Contacts.less';
+import SVG from 'utils/svg-wrap';
+import phonesImage from './assets/phones.svg';
+
+import TokenButton from '../../components/TokenButton/TokenButton';
+
 function Contacts() {
   return (
-    <div>
-      <div>asd</div>
+    <div className="Contacts">
+      <div className="Contacts__container">
+        <h2 className="Contacts__title">Subscribe our Telegram</h2>
+        <TokenButton className="Contacts__button white-btn">
+          Join Telegram
+        </TokenButton>
+        <SVG src={phonesImage} className="Contacts__image" />
+      </div>
     </div>
   );
 }
