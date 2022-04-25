9d8734609b2d069659c73dac47f16464f1eff54a
Arslan-95 <arslan180198@gmail.com>
2022-04-25 11:55:19 +0300

    [NARFEX-3] Exchange balances front end

---
+++ src/index/containers/cabinet/CabinetExchangeScreen/components/Balance/Balance.jsx
@@ -0,0 +1,136 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import SVG from 'utils/svg-wrap';
+import { Button, Search } from 'src/ui';
+import { getLang } from 'utils';
+import { openModal } from 'src/actions';
+import currencies from 'src/currencies';
+
+import Block from '../Block/Block';
+import WalletsList from '../WalletsList/WalletsList';
+import BalancesManage from './components/BalancesManage/BalancesManage';
+import WalletsListItem from '../WalletsList/components/WalletsListItem/WalletsListItem';
+import './Balance.less';
+import { connect } from 'react-redux';
+
+const Balance = React.memo(({ balances, setActiveBalance }) => {
+  // Manage page is open.
+  const [isManage, setIsManage] = React.useState(false);
+  const [isEmpty, setIsEmpty] = React.useState(false);
+  const title = isManage ? getLang('Manage') : getLang('global_balance');
+
+  React.useEffect(() => {
+    // If balances.length is false, then setIsEmpty(true).
+    setIsEmpty(!balances.filter((balance) => balance.amount).length);
+  }, []);
+
+  const handleOpenBalances = () => {
+    openModal('manage_balance');
+  };
+
+  const EmptyMessage = () => {
+    return (
+      <div className="Balances__isEmpty">
+        <p className="simple-text">{getLang('exchange_emptyBalance')}</p>
+        <Button type="lightBlue" onClick={handleOpenBalances}>
+          <SVG src={require('src/asset/120/wallet-v2.svg')} />
+          {getLang('cabinet_manage')}
+        </Button>
+      </div>
+    );
+  };
+
+  const ManageButton = () => (
+    <Button
+      className="Balances__headerButton"
+      type="secondary"
+      onClick={() => {
+        setIsManage((prevState) => !prevState);
+      }}
+      disabled={isEmpty}
+    >
+      {isManage ? (
+        <p>
+          <SVG
+            src={require('src/asset/icons/close.svg')}
+            className="isManage"
+          />
+          Close
+        </p>
+      ) : (
+        <p>
+          <SVG src={require('../../assets/settings.svg')} />
+          {getLang('Manage')}
+        </p>
+      )}
+    </Button>
+  );
+
+  return (
+    <Block
+      skipCollapse
+      className="Balances"
+      title={title}
+      controls={<ManageButton isManage={isManage} setIsManage={setIsManage} />}
+    >
+      {isManage ? (
+        <BalancesManage
+          balances={balances}
+          balancesHandler={setActiveBalance}
+          emptyMessage={isEmpty ? <EmptyMessage /> : null}
+        />
+      ) : (
+        <>
+          <Search placeholder="Search" lite simple icon disabled={isEmpty} />
+          <div className="Balances__content">
+            <WalletsList type="default" items={balances}>
+              {balances.map((item) => {
+                if (!item.currency) {
+                  return;
+                }
+
+                const { name, icon, gradient } = currencies[item.currency];
+                const iconGradient = `linear-gradient(to bottom, ${gradient[0]} 0%, ${gradient[1]} 100%)`;
+
+                return (
+                  <WalletsListItem
+                    icon={
+                      <img src={icon} style={{ background: iconGradient }} />
+                    }
+                    startTexts={[name, item.currency]}
+                    endTexts={['$' + item.price, item.amount]}
+                    key={item.id}
+                  />
+                );
+              })}
+            </WalletsList>
+            {isEmpty && <EmptyMessage />}
+          </div>
+        </>
+      )}
+    </Block>
+  );
+});
+
+Balance.propTypes = {
+  balances: PropTypes.arrayOf(
+    PropTypes.shape({
+      currency: PropTypes.string,
+      amount: PropTypes.number,
+      price: PropTypes.number,
+      id: PropTypes.number,
+      selected: PropTypes.bool,
+    })
+  ),
+  setActiveBalance: PropTypes.func,
+};
+
+Balance.defaultProps = {
+  balances: [],
+  setActiveBalance: () => {},
+};
+
+export default connect((state) => ({
+  balances: state.exchange.balances,
+}))(Balance);
