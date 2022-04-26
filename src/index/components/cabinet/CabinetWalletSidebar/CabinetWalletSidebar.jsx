807b74b7f2ed667edda54ed3bcf8c6a9a7fd0b52
Arslan-95 <arslan180198@gmail.com>
2022-04-26 17:45:27 +0300

    [NARFEX-3] Links

--- src/index/components/cabinet/CabinetWalletSidebar/CabinetWalletSidebar.jsx
+++ src/index/components/cabinet/CabinetWalletSidebar/CabinetWalletSidebar.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
+import { useRoute } from "react-router5";
 
+import * as PAGES from 'src/index/constants/pages';
 import SidebarItem from './SidebarItem/SidebarItem';
 import CabinetBlock from '../CabinetBlock/CabinetBlock';
 import './CabinetWalletSidebar.less';
 
 function CabinetWalletSidebar(props) {
+  const { route, router } = useRoute();
+
   return (
     <div className="CabinetWalletSidebar">
       <CabinetBlock>
         <ul>
           <SidebarItem title="Dashboard" icon="dashboard" />
-          <SidebarItem title="Wallet" icon="wallet" active />
-          <SidebarItem title="Exchanger" icon="exchange" />
+          <SidebarItem
+            title="Wallet"
+            icon="wallet"
+            active={route.name === PAGES.WALLET}
+            onClick={() => router.navigate(PAGES.WALLET)}
+          />
+          <SidebarItem
+            title="Exchanger"
+            icon="exchange"
+            active={route.name === PAGES.WALLET_SWAP}
+            onClick={() => router.navigate(PAGES.WALLET_SWAP)}
+          />
         </ul>
       </CabinetBlock>
       <CabinetBlock>
