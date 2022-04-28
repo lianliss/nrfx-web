31adb01174b73da973b3120b60a2589960a48a78
Arslan-95 <arslan180198@gmail.com>
2022-04-28 13:48:40 +0300

    [NARFEX-3] Cabinet Wallets jsx is ready

--- src/index/components/cabinet/CabinetWallets/CabinetWallets.jsx
+++ src/index/components/cabinet/CabinetWallets/CabinetWallets.jsx
@@ -5,6 +5,7 @@ import CabinetBlock from '../CabinetBlock/CabinetBlock';
 import SVG from 'utils/svg-wrap';
 
 import './CabinetWallets.less';
+import DynamicShadow from '../../../../ui/components/DynamicShadow/DynamicShadow';
 
 function CabinetWallets() {
   return (
@@ -16,20 +17,18 @@ function CabinetWallets() {
           token and governance token.
         </p>
         <div className="CabinetWallets__buttons">
-          <div className="dynamic-shadow">
-            <Button type="lightBlue" size="extra_large">
-              <div className="Button__icon">
-                <SVG
-                  src={require('src/asset/icons/cabinet/empty-wallet-add.svg')}
-                />
-              </div>
-              <div className="Button__content">
-                <span>Connect wallet</span>
-                <span>example wallet on MetaMask</span>
-              </div>
-            </Button>
-          </div>
-          <div className="dynamic-shadow">
+          <Button type="gray" size="extra_large">
+            <div className="Button__icon">
+              <SVG
+                src={require('src/asset/icons/cabinet/empty-wallet-add.svg')}
+              />
+            </div>
+            <div className="Button__content">
+              <span>Connect wallet</span>
+              <span>example wallet on MetaMask</span>
+            </div>
+          </Button>
+          <DynamicShadow>
             <Button type="lightBlue" size="extra_large">
               <div className="Button__icon">
                 <SVG src={require('src/asset/token/wallet.svg')} />
@@ -39,8 +38,40 @@ function CabinetWallets() {
                 <span>create wallet on our platform</span>
               </div>
             </Button>
+          </DynamicShadow>
+        </div>
+        <div className="CabinetWallets__links">
+          <a href="/" className="CabinetWallets__link">
+            learn about wallet
+            <SVG src={require('src/asset/icons/export.svg')} />
+          </a>
+          <div className="CabinetWallets-social">
+            <span className="CabinetWallets-social__title">our social</span>
+            <div className="CabinetWallets-social__items">
+              <a href="https://telegram.com">
+                <SVG
+                  src={require('src/asset/icons/social/telegram-solid.svg')}
+                  className="telegram"
+                />
+              </a>
+              <a href="https://discord.com">
+                <SVG
+                  src={require('src/asset/icons/social/discord-solid.svg')}
+                  className="discord"
+                />
+              </a>
+              <a href="https://instagram.com">
+                <SVG
+                  src={require('src/asset/icons/social/instagram-solid.svg')}
+                  className="instagram"
+                />
+              </a>
+            </div>
           </div>
         </div>
+      </div>
+      <div className="CabinetWallets-bg">
+        <img src={require('src/asset/backgrounds/cabinet-wallets-bg.png')} />
       </div>
     </CabinetBlock>
   );
