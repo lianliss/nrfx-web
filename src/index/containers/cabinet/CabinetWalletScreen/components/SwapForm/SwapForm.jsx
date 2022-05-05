f1e3294afa6f7d4ce1e8386f57c0fe98a78e6701
Arslan-95 <arslan180198@gmail.com>
2022-05-05 18:10:59 +0300

    [NARFEX-3] Add new SwapInputs

--- src/index/containers/cabinet/CabinetWalletScreen/components/SwapForm/SwapForm.jsx
+++ src/index/containers/cabinet/CabinetWalletScreen/components/SwapForm/SwapForm.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // Components
-import { Switch, SwitchTabs } from 'src/ui';
+import { Switch, SwitchTabs, Button } from 'src/ui';
 import SVG from 'utils/svg-wrap';
 import CabinetBlock from 'src/index/components/cabinet/CabinetBlock/CabinetBlock';
+import SwapFormInput from './components/SwapFormInput/SwapFormInput';
 
 // Styles
 import './SwapForm.less';
 
 function SwapForm() {
   const [isPro, setIsPro] = React.useState(false); // Pro version
+  const switchTabs = [
+    { value: 'swap', label: 'Swap' },
+    { value: 'liquidity', label: 'Liquidity' },
+    { value: 'transactions', label: 'Transactions' },
+  ];
 
   const togglePro = () => {
     setIsPro((prev) => !prev);
@@ -33,25 +39,35 @@ function SwapForm() {
           </div>
           <div className="SwapForm__row">
             <SwitchTabs
-              selected="swap"
-              tabs={[
-                { value: 'swap', label: 'Swap' },
-                { value: 'liquidity', label: 'Liquidity' },
-                { value: 'transactions', label: 'Transactions' },
-              ]}
+              selected={switchTabs[0].value}
+              tabs={switchTabs}
               onChange={() => {}}
               type="light-blue"
             />
           </div>
-          <div className="SwapForm__row">
-            <CabinetBlock>
-              <div className="SwapForm__form">e</div>
-            </CabinetBlock>
-          </div>
+        </div>
+        <div className="SwapForm__row">
+          <CabinetBlock>
+            <div className="SwapForm__form">
+              <SwapFormInput label manage title="Exchange" rate={1454.5583} />
+              <SVG
+                src={require('src/asset/icons/cabinet/swap/swap-icon.svg')}
+              />
+              <SwapFormInput label title="You Pay" rate={1454.55} />
+              <Button type="lightBlue">
+                <SVG src={require('src/asset/token/wallet.svg')} />
+                Buy on Narfex
+              </Button>
+            </div>
+          </CabinetBlock>
+        </div>
+        <div className="SwapForm__bg-center">
+          <SVG
+            src={require('src/asset/backgrounds/cabinet-swap/center-of-screen-fix.svg')}
+          />
         </div>
       </div>
       <div className="SwapForm__bg">
-        {/*<SVG src={require('src/asset/backgrounds/cabinet-swap/center-of-screen-fix.svg')} />*/}
         <SVG
           src={require('src/asset/backgrounds/cabinet-swap/right-of-screen-fix.svg')}
         />
