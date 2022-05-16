7ef49315c4f17611ff63fc3086b4a5a6d645cfd8
Danil Sakhinov <ldevil2209@ya.ru>
2022-05-17 03:24:05 +0800

    Change provider address

--- src/services/web3Provider.js
+++ src/services/web3Provider.js
@@ -50,7 +50,7 @@ class Web3Provider extends React.PureComponent {
   };
 
   ethereum = null;
-  providerAddress = 'https://nodes.pancakeswap.com:443';
+  providerAddress = 'https://bsc-dataseed1.defibit.io:443';
   factoryAddress = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73';
   routerAddress = '0x10ED43C718714eb63d5aA57B78B54704E256024E';
   wrapBNB = {
