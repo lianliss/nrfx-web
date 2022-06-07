2d33a88c0860de579badbe7b868d3c2c1f971397
Arslan-95 <arslan180198@gmail.com>
2022-06-07 14:45:14 +0300

    [Web-15] LiquidityConfirmModal adaptive is ready

--- src/index/components/cabinet/LiquidityConfirmModal/LiquidityConfirmModal.jsx
+++ src/index/components/cabinet/LiquidityConfirmModal/LiquidityConfirmModal.jsx
@@ -1,17 +1,93 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 // Components
-import { Modal } from 'src/ui';
-import CabinetBlock from '../CabinetBlock/CabinetBlock';
+import { Modal, Button, BottomSheetModal, NumberFormat } from 'src/ui';
+import { WalletIcon } from 'src/index/components/cabinet';
+import SVG from 'utils/svg-wrap';
 
 // Styles
 import './LiquidityConfirmModal.less';
 
 function LiquidityConfirmModal(props) {
+  const adaptive = useSelector((store) => store.default.adaptive);
+  const Component = adaptive ? BottomSheetModal : Modal;
+
   return (
-    <Modal className="LiquidityConfirmModal" skipClose {...props}>
-      <CabinetBlock>asd</CabinetBlock>
-    </Modal>
+    <Component
+      className="LiquidityConfirmModal"
+      prefix="LiquidityConfirmModal"
+      skipClose
+      {...props}
+    >
+      <div className="LiquidityConfirmModal__row">
+        <h2>You will receive</h2>
+        <div className="close" onClick={props.onClose}>
+          <SVG src={require('src/asset/icons/close-popup.svg')} />
+        </div>
+      </div>
+      <div className="LiquidityConfirmModal__row">
+        <span className="large-text">
+          <NumberFormat number={4.78749} />
+        </span>
+        <WalletIcon currency="nrfx" size={41} />
+        <WalletIcon currency="nrfx" size={41} />
+      </div>
+      <div className="LiquidityConfirmModal__row">
+        <p className="medium-text">USDT/DUSD Pool Tokens</p>
+      </div>
+      <div className="LiquidityConfirmModal__row">
+        <p className="small-text">
+          Output is estimated. If the price changes by more than 0.8% your
+          transaction will revert.
+        </p>
+      </div>
+      <div className="LiquidityConfirmModal__row">
+        <div className="LiquidityConfirmModal__result">
+          <div className="LiquidityConfirmModal__item">
+            <span className="default-text-dark">USDT Deposited</span>
+            <span className="default-text-dark">
+              <WalletIcon currency="nrfx" size={24} marginRight={10} />
+              <NumberFormat number={4.99437} />
+            </span>
+          </div>
+          <div className="LiquidityConfirmModal__item">
+            <span className="default-text-dark">BUSD Deposited</span>
+            <span className="default-text-dark">
+              <WalletIcon currency="nrfx" size={24} marginRight={10} />
+              <NumberFormat number={4.99437} />
+            </span>
+          </div>
+          <div className="LiquidityConfirmModal__item">
+            <span className="default-text-dark">Rates</span>
+            <span className="default-text-dark">
+              <NumberFormat number={15.05} currency="usdt" />
+              &nbsp;=&nbsp;
+              <NumberFormat number={15.05} currency="busd" />
+            </span>
+          </div>
+          <div className="LiquidityConfirmModal__item">
+            <span className="default-text-dark"></span>
+            <span className="default-text-dark">
+              <NumberFormat number={15.05} currency="usdt" />
+              &nbsp;=&nbsp;
+              <NumberFormat number={15.05} currency="busd" />
+            </span>
+          </div>
+          <div className="LiquidityConfirmModal__item">
+            <span className="default-text-dark">Share of Pool:</span>
+            <span className="default-text-dark">
+              <NumberFormat number={0.01653} percent />
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="LiquidityConfirmModal__row">
+        <Button size="extra_large" type="lightBlue">
+          Confirm Suppy
+        </Button>
+      </div>
+    </Component>
   );
 }
 
---
+++ src/index/components/cabinet/index.js
@@ -0,0 +1,3 @@
+export { default as WalletIcon } from './WalletIcon/WalletIcon';
+export { default as DoubleWallets } from './DoubleWallets/DoubleWallets';
+export { default as LiquidityConfirmModal } from './LiquidityConfirmModal/LiquidityConfirmModal';
