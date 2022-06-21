5324b7f286830208afaa21127f18988b4fda777e
Arslan-95 <arslan180198@gmail.com>
2022-06-21 19:30:56 +0300

    Dapp Currency header is ready

---
+++ src/index/components/dapp/Currency/Currency.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+// Components
+import CabinetBlock from '../CabinetBlock/CabinetBlock';
+import { WalletIcon } from '../index';
+import { NumberFormat } from 'src/ui';
+import SVG from 'utils/svg-wrap';
+
+// Utils
+import currencies from 'src/currencies';
+
+// Styles
+import './Currency.less';
+
+function Currency() {
+  const params = useSelector((state) => state.router.route.params);
+
+  // Get currency object
+  const currency = currencies[params.currency] || null;
+
+  if (!currency) {
+    return (
+      <div className="Currency">
+        <div>{params.currency.toUpperCase()} is not exists</div>
+      </div>
+    );
+  }
+
+  return (
+    <CabinetBlock className="Currency">
+      <div className="Currency__container">
+        <div className="Currency__header">
+          <div className="Currency__preview">
+            <WalletIcon currency={currency.abbr} size={41} />
+            <span className="Currency__currency">{currency.name}</span>
+            <span className="Currency__rate">12 USD</span>
+            <div className="Currency__currency_amount">
+              <NumberFormat number={155} currency={currency.abbr} />
+            </div>
+            <div className="Currency__currency_amount_rate">
+              <NumberFormat number={54.0} currency={'usd'} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </CabinetBlock>
+  );
+}
+
+export default Currency;
