525defca0fa310a4813c8b2e023640fae8aa7f5f
Arslan-95 <arslan180198@gmail.com>
2022-06-29 15:45:44 +0300

    Fix social links

--- src/index/components/dapp/SocialLinks/SocialLinks.jsx
+++ src/index/components/dapp/SocialLinks/SocialLinks.jsx
@@ -4,12 +4,17 @@ import React from 'react';
 import SVG from 'utils/svg-wrap';
 import OpenPopupLink from '../OpenPopupLink/OpenPopupLink';
 
+// Utils
+import COMPANY from '../../../constants/company';
+
 // Styles
 import './SocialLinks.less';
 
 function SocialLinks() {
+  const { social } = COMPANY;
+
   const LinkIcon = ({ icon, to, className }) => (
-    <a href={to} target="_blank">
+    <a href={`//${to}`} target="_blank">
       <SVG
         src={require(`src/asset/icons/social/${icon}.svg`)}
         className={className}
@@ -25,21 +30,12 @@ function SocialLinks() {
       <div className="SocialLinks-social">
         <span className="SocialLinks-social__title">our social</span>
         <div className="SocialLinks-social__items">
-          <LinkIcon to="https://t.me/narfexfeed" icon="telegram-solid" />
-          <LinkIcon to="https://discord.gg/T4hFnUaPFS" icon="discord-solid" />
-          <LinkIcon
-            to="https://instagram.com/narfex.global"
-            icon="instagram-solid"
-          />
-          <LinkIcon to="https://medium.com/@narfex" icon="M-solid" />
-          <LinkIcon
-            to="https://www.youtube.com/channel/UCDwJ0XUJDJpQAhB9DxYYUlw"
-            icon="youtube-solid"
-          />
-          <LinkIcon
-            to="https://twitter.com/narfexglobal?s=21"
-            icon="twitter-solid"
-          />
+          <LinkIcon to="t.me/narfexfeed" icon="telegram-solid" />
+          <LinkIcon to={social.discord} icon="discord-solid" />
+          <LinkIcon to={social.instagram} icon="instagram-solid" />
+          <LinkIcon to={social.medium} icon="M-solid" />
+          <LinkIcon to={social.youtube} icon="youtube-solid" />
+          <LinkIcon to={social.twitter} icon="twitter-solid" />
         </div>
       </div>
     </div>
