fefa4b5373fd3f4736b31baa239bf992d272c988
Arslan-95 <arslan180198@gmail.com>
2022-07-01 14:17:20 +0300

    [Web-26] Adaptive and Button Show more is ready

--- src/index/components/dapp/Referral/components/ReferralList/ReferralList.jsx
+++ src/index/components/dapp/Referral/components/ReferralList/ReferralList.jsx
@@ -9,14 +9,79 @@ import SVG from 'utils/svg-wrap';
 import './ReferralList.less';
 
 const testItems = [
-  { login: 'Login', total: { nrfx: '212', usd: '21' } },
-  { login: 'Login', total: { nrfx: '212', usd: '21' } },
-  { login: 'Login', total: { nrfx: '212', usd: '21' } },
-  { login: 'Login', total: { nrfx: '212', usd: '21' } },
-  { login: 'Login', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 1', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 2', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 3', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 4', total: { nrfx: '2121212', usd: '21122212121111' } },
+  { login: 'Login 5', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 6', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 7', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 8', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 9', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 10', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212122', usd: '21211' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
+  { login: 'Login 11', total: { nrfx: '212', usd: '21' } },
 ];
 
 function ReferralList({ title, subtitle, items }) {
+  const [visibleNumber, setVisibleNumber] = React.useState(10);
+
   return (
     <div className="Referral__ReferralList">
       <Row alignItems="flex-end" justifyContent="space-between">
@@ -39,28 +104,45 @@ function ReferralList({ title, subtitle, items }) {
             </Col>
           </Row>
         ) : (
-          <Col className="Referral__ReferralList__items-exists">
-            <Row justifyContent="space-between" className="title-row">
-              <Col>Partner</Col>
-              <Col>Total NRFX earned</Col>
-              <Col>Total Fiat earned</Col>
-            </Row>
-            {items.map((item, index) => (
-              <Row
-                justifyContent="space-between"
-                alignItems="center"
-                key={item.login + index}
-              >
-                <Col>{item.login}</Col>
-                <Col>
-                  <NumberFormat number={item.total.nrfx} currency="nrfx" />
-                </Col>
-                <Col>
-                  <NumberFormat number={item.total.usd} currency="usd" />
-                </Col>
+          <>
+            <Col className="Referral__ReferralList__items-exists">
+              <Row justifyContent="space-between" className="title-row">
+                <Col>Partner</Col>
+                <Col>Total NRFX earned</Col>
+                <Col>Total Fiat earned</Col>
               </Row>
-            ))}
-          </Col>
+              {items.slice(0, visibleNumber).map((item, index) => (
+                <Row
+                  justifyContent="space-between"
+                  alignItems="center"
+                  key={item.login + index}
+                >
+                  <Col>{item.login}</Col>
+                  <Col>
+                    <NumberFormat number={item.total.nrfx} currency="nrfx" />
+                  </Col>
+                  <Col>
+                    <NumberFormat number={item.total.usd} currency="usd" />
+                  </Col>
+                </Row>
+              ))}
+            </Col>
+            <Row alignItems="center" className="smoothShow">
+              {visibleNumber <= items.length && (
+                <div
+                  onClick={() => {
+                    setVisibleNumber((prevState) => prevState + 10);
+                  }}
+                  className="show-more"
+                >
+                  <span>Show more</span>
+                  <SVG
+                    src={require('src/asset/icons/arrows/form-dropdown.svg')}
+                  />
+                </div>
+              )}
+            </Row>
+          </>
         )}
       </Col>
     </div>
