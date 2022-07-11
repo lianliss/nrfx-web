a5109b0dda46864ce176f586cbbf994cc74f59d1
Arslan-95 <arslan180198@gmail.com>
2022-07-11 13:51:36 +0300

    openCabinetModal clean code and Fix Slippage modal bug

--- src/index/components/dapp/Modals/ConnectToWalletModal/hooks/openCabinetModal.js
+++ src/index/components/dapp/Modals/ConnectToWalletModal/hooks/openCabinetModal.js
@@ -2,25 +2,24 @@ import React from 'react';
 
 export const openCabinetModal = () => {
   React.useEffect(() => {
-    const dappContent = document.querySelector('.DappWrapper__content');
+    const dappWrapper = document.querySelector('.DappWrapper__content');
+    const dappContent = document.querySelector('.DappContainer__content');
+    const pageWrapper = document.querySelector('.CabinetWrapper__content');
     const pageContent = document.querySelector('.PageContainer__content');
+    const contentBlocks = [dappWrapper, dappContent, pageWrapper, pageContent];
 
-    if (pageContent) {
-      pageContent.style.zIndex = 100;
-    }
-
-    if (dappContent) {
-      dappContent.style.zIndex = 100;
-    }
+    contentBlocks.forEach((item) => {
+      if (item) {
+        item.style.zIndex = 100;
+      }
+    });
 
     return () => {
-      if (pageContent) {
-        pageContent.style.zIndex = 1;
-      }
-
-      if (dappContent) {
-        dappContent.style.zIndex = 1;
-      }
+      contentBlocks.forEach((item) => {
+        if (item) {
+          item.style.zIndex = 1;
+        }
+      });
     };
   }, []);
 
