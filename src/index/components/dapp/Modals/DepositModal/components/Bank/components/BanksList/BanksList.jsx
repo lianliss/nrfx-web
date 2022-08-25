91e6a705a4d32e47c2f82c5c4382e44bfeae2328
Arslan-95 <arslan180198@gmail.com>
2022-08-25 14:12:36 +0300

    Choose bank withdrawal is ready

---
+++ src/index/components/dapp/Modals/DepositModal/components/Bank/components/BanksList/BanksList.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import CabinetScrollBlock from 'src/index/components/dapp/CabinetScrollBlock/CabinetScrollBlock';
+import { Row, Col } from 'src/ui';
+import SVG from 'utils/svg-wrap';
+
+import './BanksList.less';
+
+function BanksList({ items, onChange, adaptive }) {
+  const BanksWrapper = items.length > 5 && !adaptive ? CabinetScrollBlock : Col;
+
+  return (
+    <BanksWrapper className="DepositModal__ChooseBank-items" noScrollX>
+      {items.map((bank, key) => {
+        let icon = null;
+
+        try {
+          icon = require(`src/asset/banks/${bank.code}.svg`).default;
+        } catch {
+          console.log('[BankList] Logo is not defined');
+        }
+
+        return (
+          <Row
+            className="DepositModal__ChooseBank-item"
+            alignItems="center"
+            onClick={() => onChange(bank)}
+            key={key}
+          >
+            <div>
+              <img src={icon} alt={bank.name} className="bankIcon" />
+            </div>
+            <Row alignItems="center" justifyContent="flex-end">
+              <span className="secondary extra-small default">{bank.name}</span>
+              <SVG src={require('src/asset/icons/list-arrow-large.svg')} />
+            </Row>
+          </Row>
+        );
+      })}
+    </BanksWrapper>
+  );
+}
+
+BanksList.propTypes = {
+  items: PropTypes.array,
+  onChange: PropTypes.func,
+  adaptive: PropTypes.bool,
+};
+
+BanksList.defaultProps = {
+  items: [],
+  onChange: () => {},
+  adaptive: false,
+};
+
+export default BanksList;
---
+++ src/index/components/dapp/Modals/DepositModal/components/Bank/components/ChooseBank/containers/Choose/Choose.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Row, Col, Button } from 'src/ui';
+import SVG from 'utils/svg-wrap';
+import Lang from 'src/components/Lang/Lang';
+import BanksList from '../../../BanksList/BanksList';
+import { classNames } from 'src/utils';
+
+import './Choose.less';
+
+function Choose({ items, onChange, onBack, adaptive, type }) {
+  const className = classNames({
+    DepositModal__ChooseBank: true,
+    [type]: type,
+  });
+
+  return (
+    <Col className={className}>
+      {items.length ? (
+        <>
+          <Row
+            alignItems="center"
+            justifyContent="space-between"
+            className="DepositModal__ChooseBank__title"
+          >
+            <h3 className="default dark medium">
+              <Lang name="cabinet_fiatWithdrawalModal_chooseBank" />
+            </h3>
+            {type === 'withdrawal' && (
+              <span
+                className="DepositModal__ChooseBank__close"
+                onClick={onBack}
+              >
+                <SVG src={require('src/asset/icons/close-popup.svg')} />
+              </span>
+            )}
+          </Row>
+          {items ? (
+            <BanksList
+              adaptive={adaptive}
+              onChange={(b) => onChange(b.code)}
+              items={items}
+            />
+          ) : (
+            <LoadingStatus status={'loading'} />
+          )}
+        </>
+      ) : (
+        <div className="DepositModal__ChooseBank__empty">
+          <h3 className="default blue extra-large-height">
+            <Lang name="fiatRefillCard_status_not_available_cards" />
+          </h3>
+          <SVG src={require('src/asset/icons/transaction/empty-icon.svg')} />
+        </div>
+      )}
+      {type !== 'withdrawal' && (
+        <Row className="buttons" justifyContent="flex-end">
+          <Button type="secondary-alice" shadow onClick={onBack}>
+            <Lang name="global_back" />
+          </Button>
+        </Row>
+      )}
+    </Col>
+  );
+}
+
+Choose.propTypes = {
+  items: PropTypes.array,
+  onChange: PropTypes.func,
+  adaptive: PropTypes.bool,
+  type: PropTypes.oneOf(['refill', 'withdrawal']),
+  onBack: PropTypes.func,
+};
+
+Choose.defaultProps = {
+  items: [],
+  onChange: () => {},
+  adaptive: false,
+  type: 'refill',
+  onBack: () => {},
+};
+
+export default Choose;
