382450267c90a8cf272444811a4787d0296dc844
Arslan-95 <arslan180198@gmail.com>
2022-09-01 15:47:02 +0300

    App Buttons

--- src/components/AppButtons/AppButtons.js
+++ src/components/AppButtons/AppButtons.js
@@ -8,14 +8,15 @@ import Lang from "../Lang/Lang";
 export default ({ className }) => {
   return (
     <div className={cn("AppButtons", className)}>
-      <a
-        href={company.apps.android}
-        target="_blank"
+      <div
         rel="noopener noreferrer"
-        className="AppButtons__button"
+        className="AppButtons__button disabled"
       >
+        <i>
+          <Lang name="global_comingSoon" />
+        </i>
         <SVG src={require("src/asset/app_markets/google_play.svg")} />
-      </a>
+      </div>
       <div className="AppButtons__button disabled">
         <i>
           <Lang name="global_comingSoon" />
