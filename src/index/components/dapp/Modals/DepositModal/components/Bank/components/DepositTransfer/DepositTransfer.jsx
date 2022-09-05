85a98a6de1a935b17e68a0c5ad5092632ef45125
Arslan-95 <arslan180198@gmail.com>
2022-09-05 10:57:51 +0300

    Add Langs to submitted modals

--- src/index/components/dapp/Modals/DepositModal/components/Bank/components/DepositTransfer/DepositTransfer.jsx
+++ src/index/components/dapp/Modals/DepositModal/components/Bank/components/DepositTransfer/DepositTransfer.jsx
@@ -11,7 +11,7 @@ import Lang from 'src/components/Lang/Lang';
 // Styles
 import './DepositTransfer.less';
 
-function DepositTransfer({ onClose, adaptive }) {
+function DepositTransfer({ onClose, adaptive, text }) {
   const context = React.useContext(Web3Context);
   const { chainId, accountAddress, addTokenToWallet, fiats } = context;
   const routerState = router.getState();
@@ -33,10 +33,7 @@ function DepositTransfer({ onClose, adaptive }) {
       <h3 className="blue large-height">
         <Lang name="fiatRefillCard_status_wait_for_review" />
       </h3>
-      <p className="DepositModal__Bank__DepositTransfer-subtitle">
-        Your tokens will be credited directly to your connected wallet. Import
-        the token into your wallet using the button below.
-      </p>
+      <p className="DepositModal__Bank__DepositTransfer-subtitle">{text}</p>
       <div className="speed-icon">
         <SVG src={require('src/asset/icons/transaction/speed.svg')} />
       </div>
