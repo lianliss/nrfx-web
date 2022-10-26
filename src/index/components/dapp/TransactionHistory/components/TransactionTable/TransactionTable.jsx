144df59a52fc95d66820e9ab20dfce359df58c26
Arslan-95 <arslan180198@gmail.com>
2022-10-26 16:07:45 +0300

    NRFX-56 sort icon is added

--- src/index/components/dapp/TransactionHistory/components/TransactionTable/TransactionTable.jsx
+++ src/index/components/dapp/TransactionHistory/components/TransactionTable/TransactionTable.jsx
@@ -4,13 +4,19 @@ import PropTypes from 'prop-types';
 // Components
 import CabinetTable, { TD, TR } from '../../../CabinetTable/CabinetTable';
 import Transaction from './components/Transaction';
+import SVG from 'utils/svg-wrap';
 
 function TransactionTable({ accountHistory }) {
   return (
     <CabinetTable
       header={
         <TR>
-          <TD>Date</TD>
+          <TD>
+            Date
+            <span className="TransactionHistory__icon-sortArrow">
+              <SVG src={require('src/asset/icons/cabinet/select-arrow.svg')} />
+            </span>
+          </TD>
           <TD>Operation</TD>
           <TD>Currency</TD>
           <TD>Status</TD>
