3f68ccd7e4a5830f0a95c726844e40f47bfa8803
Arslan-95 <arslan180198@gmail.com>
2022-10-27 14:11:26 +0300

    NRFX-87 Intercom button is working

--- src/index/components/dapp/Modals/components/HaveAQuestion/HaveAQuestion.jsx
+++ src/index/components/dapp/Modals/components/HaveAQuestion/HaveAQuestion.jsx
@@ -3,10 +3,55 @@ import React from 'react';
 // Components
 import { Button } from 'src/ui';
 
+// Utils
+import _ from 'lodash';
+
 // Styles
 import './HaveAQuestion.less';
 
 function HaveAQuestion() {
+  const [intercomIsVisible, setIntercomIsVisible] = React.useState(false);
+  const [isListener, setIsListener] = React.useState(false);
+
+  const addListenersToIntercom = () => {
+    // if listener is added
+    // or Intercom is not defined
+    if (isListener) return;
+    if (!_.isFunction(window.Intercom)) return;
+
+    window.Intercom('onShow', () => {
+      setIntercomIsVisible(true);
+    });
+
+    window.Intercom('onHide', () => {
+      setIntercomIsVisible(false);
+    });
+
+    // Add listeners to Intercom
+    setIsListener(true);
+  };
+
+  const chatToggler = () => {
+    if (!_.isFunction(window.Intercom)) return;
+
+    // Hide intercom chat if visible
+    if (intercomIsVisible) {
+      window.Intercom('hide');
+      setIntercomIsVisible(false);
+
+      return;
+    }
+
+    // Show intercome chat
+    window.Intercom('show');
+    setIntercomIsVisible(true);
+  };
+
+  const chatHandler = () => {
+    chatToggler();
+    addListenersToIntercom();
+  };
+
   return (
     <div className="HaveAQuestion">
       <p className="HaveAQuestion__title">
@@ -23,7 +68,12 @@ function HaveAQuestion() {
         </a>
       </div>
       <div className="HaveAQuestion__button">
-        <Button size="middle" type="secondary-alice" shadow>
+        <Button
+          size="middle"
+          type="secondary-alice"
+          shadow
+          onClick={chatHandler}
+        >
           Online chat
         </Button>
       </div>
