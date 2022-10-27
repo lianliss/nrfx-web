4030e2f2534465bf112450726666b030fe914ac3
Arslan-95 <arslan180198@gmail.com>
2022-10-27 12:22:53 +0300

    NRFX-87 Telegram link is added

--- src/index/components/dapp/Modals/components/HaveAQuestion/HaveAQuestion.jsx
+++ src/index/components/dapp/Modals/components/HaveAQuestion/HaveAQuestion.jsx
@@ -15,12 +15,18 @@ function HaveAQuestion() {
           via:
         </em>
       </p>
-      <Button size="middle" type="secondary-alice" shadow>
-        Telegram
-      </Button>
-      <Button size="middle" type="secondary-alice" shadow>
-        Online chat
-      </Button>
+      <div className="HaveAQuestion__button">
+        <a href="https://t.me/NarfexExchange" target="_blank">
+          <Button size="middle" type="secondary-alice" shadow>
+            Telegram
+          </Button>
+        </a>
+      </div>
+      <div className="HaveAQuestion__button">
+        <Button size="middle" type="secondary-alice" shadow>
+          Online chat
+        </Button>
+      </div>
     </div>
   );
 }
