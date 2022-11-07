b0e2b869f34b2b7e3dd375bdd2b0623e59df3592
Arslan-95 <arslan180198@gmail.com>
2022-11-07 18:10:14 +0300

    NRFX-95 Coming soon nft

--- src/index/components/dapp/CabinetWallets/components/WalletsExists/components/NftsBlock/NftsBlock.jsx
+++ src/index/components/dapp/CabinetWallets/components/WalletsExists/components/NftsBlock/NftsBlock.jsx
@@ -4,9 +4,16 @@ import React from 'react';
 import CabinetBlock from '../../../../../CabinetBlock/CabinetBlock';
 import CabinetScrollBlock from '../../../../../CabinetScrollBlock/CabinetScrollBlock';
 import OpenPopupLink from '../../../../../OpenPopupLink/OpenPopupLink';
+import Overlay from '../../../../../ui/Overlay/Overlay';
 import WalletsNFTCard from '../../../WalletsNFTCard/WalletsNFTCard';
+import SVG from 'utils/svg-wrap';
+
+// Utils
+import { getLang } from 'utils';
 
 function NftsBlock({ adaptive }) {
+  const NFTsExists = false;
+
   return (
     <CabinetBlock className="nfts">
       {!adaptive && (
@@ -17,22 +24,34 @@ function NftsBlock({ adaptive }) {
       )}
       <CabinetScrollBlock disableTrackXMousewheelScrolling>
         <div className="WalletsNFT__cards">
-          <WalletsNFTCard title="Monkey" src={'1'} />
-          <WalletsNFTCard title="Hello Kitty 1445" src={'2'} />
-          <WalletsNFTCard title="Degen Ape 6" src={'4'} />
-          <WalletsNFTCard title="Degen Ape 6" src={'5'} />
-          <WalletsNFTCard title="Brod 45" src={'3'} />
-          <WalletsNFTCard title="Monkey" src={'1'} />
-          <WalletsNFTCard title="Hello Kitty 1445" src={'2'} />
-          <WalletsNFTCard title="Degen Ape 6" src={'4'} />
-          <WalletsNFTCard title="Degen Ape 6" src={'5'} />
-          <WalletsNFTCard title="Brod 45" src={'3'} />
-          <WalletsNFTCard title="Hello Kitty 1445" src={'2'} />
-          <WalletsNFTCard title="Degen Ape 6" src={'5'} />
-          <WalletsNFTCard title="Brod 45" src={'3'} />
+          {NFTsExists ? (
+            <>
+              <WalletsNFTCard title="Monkey" src={'1'} />
+              <WalletsNFTCard title="Hello Kitty 1445" src={'2'} />
+              <WalletsNFTCard title="Degen Ape 6" src={'4'} />
+              <WalletsNFTCard title="Degen Ape 6" src={'5'} />
+              <WalletsNFTCard title="Brod 45" src={'3'} />
+              <WalletsNFTCard title="Monkey" src={'1'} />
+              <WalletsNFTCard title="Hello Kitty 1445" src={'2'} />
+              <WalletsNFTCard title="Degen Ape 6" src={'4'} />
+              <WalletsNFTCard title="Degen Ape 6" src={'5'} />
+              <WalletsNFTCard title="Brod 45" src={'3'} />
+              <WalletsNFTCard title="Hello Kitty 1445" src={'2'} />
+              <WalletsNFTCard title="Degen Ape 6" src={'5'} />
+              <WalletsNFTCard title="Brod 45" src={'3'} />
+            </>
+          ) : (
+            Array(6)
+              .fill({})
+              .map((_, key) => <WalletsNFTCard key={key} />)
+          )}
         </div>
       </CabinetScrollBlock>
       {adaptive && <div className="WalletsExists__items_footer"></div>}
+      <Overlay>
+        <SVG src={require('src/asset/icons/status/empty-image.svg')} />
+        <h3>{getLang('global_comingSoon')}</h3>
+      </Overlay>
     </CabinetBlock>
   );
 }
--- src/index/components/dapp/ui/Overlay/Overlay.jsx
+++ src/index/components/dapp/ui/Overlay/Overlay.jsx
@@ -3,10 +3,13 @@ import React from 'react';
 // Styles
 import './Overlay.less';
 
-function Overlay({ children }) {
+function Overlay({ children, backdropFilter, background }) {
   return (
     <div className="DappUI__Overlay">
-      <div className="DappUI__Overlay__bg" />
+      <div
+        className="DappUI__Overlay__bg"
+        style={{ backdropFilter, background }}
+      />
       <div className="DappUI__Overlay__container">{children}</div>
     </div>
   );
