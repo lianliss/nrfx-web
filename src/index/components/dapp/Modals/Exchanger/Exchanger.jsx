4341dd497db9b37c25a9282d602a59161b47a37c
Arslan-95 <arslan180198@gmail.com>
2022-11-24 16:46:49 +0300

    NRFX-89 Modal setup

---
+++ src/index/components/dapp/Modals/Exchanger/Exchanger.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Components
+import { CabinetModal } from 'dapp';
+
+// Styles
+import './Exchanger.less';
+
+function Exchanger() {
+  return <CabinetModal className="ExchangerModal">Exchanger</CabinetModal>;
+}
+
+export default Exchanger;
