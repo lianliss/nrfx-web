f8196991e8d288b203e232362442ee6c7f714b94
Arslan-95 <arslan180198@gmail.com>
2022-12-01 16:32:59 +0300

    NRFX-120 Preview page is ready

--- src/index/containers/validator/Preview/Preview.jsx
+++ src/index/containers/validator/Preview/Preview.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 // Components
 import { Button, Col } from 'ui';
+import SVG from 'utils/svg-wrap';
 import CabinetBlock from '../../../components/dapp/CabinetBlock/CabinetBlock';
 import SocialLinks from '../../../components/dapp/SocialLinks/SocialLinks';
+import BackgroundBit from '../../../components/validator/BackgroundBit/BackgroundBit';
+
+// Icons
+import blueCross from 'src/asset/backgrounds/bg-bit/blue-cross.svg';
+import blueWaveLine from 'src/asset/backgrounds/bg-bit/blue-wave-line.svg';
+import blueCircle from 'src/asset/backgrounds/bg-bit/blue-circle.svg';
 
 // Styles
 import './Preview.less';
@@ -27,6 +34,42 @@ function Preview() {
               Verify
             </Button>
           </div>
+        </div>
+        <div className="ValidatorPreview__background">
+          <SVG
+            src={require('src/asset/backgrounds/validator-preview-bg.svg')}
+          />
+          <BackgroundBit
+            src={blueCross}
+            left={-73}
+            top={131}
+            width={18}
+            rotate={4}
+          />
+          <BackgroundBit
+            src={blueCross}
+            width={16.44}
+            top={244}
+            right={-42}
+            rotate={12}
+          />
+          <BackgroundBit
+            src={blueCross}
+            width={17.1}
+            top={374}
+            left={-133}
+            rotate={-23.74}
+          />
+          <BackgroundBit
+            src={blueCircle}
+            top={26}
+            right={-38}
+            width={26}
+            height={26}
+          />
+          <BackgroundBit src={blueCircle} bottom={8} left={1} />
+          <BackgroundBit src={blueWaveLine} top={4} left={-57} rotate={32.8} />
+          <BackgroundBit src={blueWaveLine} bottom={8} right={-52} />
         </div>
       </CabinetBlock>
       <SocialLinks />
