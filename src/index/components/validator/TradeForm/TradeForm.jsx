e7c4a71c056cca681ad887bb5e3e7c31a3578a8e
Arslan-95 <arslan180198@gmail.com>
2022-12-02 17:48:30 +0300

    NRFX-120 Radio inputs is ready

--- src/index/components/validator/TradeForm/TradeForm.jsx
+++ src/index/components/validator/TradeForm/TradeForm.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 // Components
-import { Form, Radio, RadioGroup } from 'ui';
+import { Form, Radio, RadioGroup, Col, Row } from 'ui';
+import { AnswerPopup } from 'dapp';
 
 // Utils
 
@@ -14,16 +15,25 @@ function TradeForm() {
   return (
     <Form className="ValidatorTradeForm">
       <h2>Trade type</h2>
-      <div>
-        <RadioGroup selected={tradeType} onChange={setTradeType}>
-          <Radio size="small" type="light-blue" value="sell">
-            Sell bitcoins online
-          </Radio>
-          <Radio size="small" type="light-blue" value="buy">
-            Buy bitcoins online
-          </Radio>
-        </RadioGroup>
-      </div>
+      <Row className="ValidatorTradeForm-row">
+        <Col className="ValidatorTradeForm-col">
+          <Row className="ValidatorTradeForm-col__title" alignItems="center">
+            <h3>I want to...</h3>
+            <AnswerPopup>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+              quidem explicabo alias.
+            </AnswerPopup>
+          </Row>
+          <RadioGroup selected={tradeType} onChange={setTradeType}>
+            <Radio size="small" type="light-blue" value="sell">
+              Sell bitcoins online
+            </Radio>
+            <Radio size="small" type="light-blue" value="buy">
+              Buy bitcoins online
+            </Radio>
+          </RadioGroup>
+        </Col>
+      </Row>
     </Form>
   );
 }
