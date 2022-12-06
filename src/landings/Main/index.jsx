fdc0fb63bb29781dc24f605409daed48c4535166
Arslan-95 <arslan180198@gmail.com>
2022-12-06 11:44:57 +0300

    Routes changed

---
+++ src/landings/Main/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Skeleton from '../../ui/components/Skeleton/Skeleton';
+
+// Components
+
+// Styles
+import './index.less';
+
+function Main() {
+  return (
+    <div className="MainLanding">
+      <div className="MainLanding-first-screen">
+      </div>
+    </div>
+  );
+}
+
+export default Main;
---
+++ src/wrappers/MainLanding/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Styles
+import './index.less';
+
+function MainLandingWrapper({ children }) {
+  return <div className="MainLandingWrapper">{children}</div>;
+}
+
+export default MainLandingWrapper;
