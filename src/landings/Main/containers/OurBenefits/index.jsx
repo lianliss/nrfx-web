01b5ff812223d8cd5bc0ea42013431ce96028467
Arslan-95 <arslan180198@gmail.com>
2022-12-09 19:06:34 +0300

    NRFX-7 OurBenefits container is created

---
+++ src/landings/Main/containers/OurBenefits/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OurBenefits() {
+  return (
+    <div className="MainLanding-our-benefits">
+      <h2>Our Benefits</h2>
+    </div>
+  );
+}
+
+export default OurBenefits;
