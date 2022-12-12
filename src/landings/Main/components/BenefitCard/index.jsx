49a677f5129839c959ebb77948f6ca6a434d0481
Arslan-95 <arslan180198@gmail.com>
2022-12-12 14:36:43 +0300

    NRFX-7 Benefits titles is ready

--- src/landings/Main/components/BenefitCard/index.jsx
+++ src/landings/Main/components/BenefitCard/index.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+// Components
+
+// Utils
+import { classNames as cn } from 'utils';
 
 // Styles
 import './index.less';
 
-function BenefitCard() {
-  return <div>BenefitCard</div>;
+function BenefitCard({ size, background, image, title }) {
+  return (
+    <div className={cn('MainLanding-BenefitCard', size, background)}>
+      <p className="MainLanding-BenefitCard__title">{title}</p>
+    </div>
+  );
 }
+
+BenefitCard.propTypes = {
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  background: PropTypes.oneOf(['blue', 'alice-blue', 'orange']),
+};
+
+BenefitCard.defaultProps = {
+  size: 'small',
+  background: 'alice-blue',
+};
 
 export default BenefitCard;
--- src/landings/Main/components/BenefitCards/index.jsx
+++ src/landings/Main/components/BenefitCards/index.jsx
@@ -1,10 +1,53 @@
 import React from 'react';
 
+// Components
+import { Col, Row } from 'ui';
+import BenefitCard from '../BenefitCard';
+
+// Utils
+import { getLang } from 'utils';
+
 // Styles
 import './index.less';
 
 function BenefitCards() {
-  return <div>BenefitCards</div>;
+  return (
+    <Col
+      className="MainLanding-BenefitCards"
+      alignItems="stretch"
+      justifyContent="stretch"
+    >
+      <Row
+        alignItems="stretch"
+        justifyContent="stretch"
+        className="MainLanding-BenefitCards__row"
+      >
+        <BenefitCard
+          title="Trustless & Based on smart contracts"
+          size="medium"
+          background="blue"
+        />
+        <BenefitCard title="users" size="small" />
+        <BenefitCard
+          title={getLang('main_landing_benefits_ecosystem')}
+          size="medium"
+        />
+      </Row>
+      <Row
+        alignItems="stretch"
+        justifyContent="stretch"
+        className="MainLanding-BenefitCards__row"
+      >
+        <BenefitCard title="exchange transactions every day" size="large" />
+        <BenefitCard
+          title="50+ fiat currencies"
+          size="small"
+          background="orange"
+        />
+        <BenefitCard title="Community driven (DAO)" size="small" />
+      </Row>
+    </Col>
+  );
 }
 
 export default BenefitCards;
