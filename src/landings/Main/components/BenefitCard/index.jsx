cd4c406bcd0d92c8089bddf1c9e9d7ede03143e9
Arslan-95 <arslan180198@gmail.com>
2022-12-12 13:36:00 +0300

    NRFX-7 Benefit cards component created

---
+++ src/landings/Main/components/BenefitCard/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Styles
+import './index.less';
+
+function BenefitCard() {
+  return <div>BenefitCard</div>;
+}
+
+export default BenefitCard;
---
+++ src/landings/Main/components/BenefitCards/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Styles
+import './index.less';
+
+function BenefitCards() {
+  return <div>BenefitCards</div>;
+}
+
+export default BenefitCards;
