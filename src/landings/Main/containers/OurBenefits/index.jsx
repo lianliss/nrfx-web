d7b0db4f1e4aff55d4f176ba67d9f3710017b50d
Arslan-95 <arslan180198@gmail.com>
2022-12-12 13:35:55 +0300

    NRFX-7 Benefit cards component created

--- src/landings/Main/containers/OurBenefits/index.jsx
+++ src/landings/Main/containers/OurBenefits/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
+// Components
+import { Container } from 'ui';
+import BenefitCards from '../../components/BenefitCards';
+
+// Styles
+import './index.less';
+
 function OurBenefits() {
   return (
-    <div className="MainLanding-our-benefits">
-      <h2>Our Benefits</h2>
-    </div>
+    <Container maxWidth={1356} padding={22}>
+      <div className="MainLanding-our-benefits">
+        <h2>Our Benefits</h2>
+        <BenefitCards />
+      </div>
+    </Container>
   );
 }
 
