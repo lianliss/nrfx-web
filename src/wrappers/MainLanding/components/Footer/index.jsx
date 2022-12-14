02636335eda2255bd4e8811d0a707aaca7dfc99a
Arslan-95 <arslan180198@gmail.com>
2022-12-14 21:14:22 +0300

    NRFX-7 Footer nav links is ready

--- src/wrappers/MainLanding/components/Footer/index.jsx
+++ src/wrappers/MainLanding/components/Footer/index.jsx
@@ -1,7 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router5';
+
+// Components
+import { Row, Col, Container } from 'ui';
+
+// Styles
+import './index.less';
 
 function Footer({ adaptive }) {
-  return <div className="MainLandingWrapperFooter"></div>;
+  return (
+    <footer className="MainLandingWrapperFooter">
+      <Container maxWidth={1293} padding={adaptive ? 15 : 38}>
+        <Row
+          className="MainLandingWrapperFooter__main"
+          justifyContent="space-between"
+        >
+          <nav className="MainLandingWrapperFooter-nav">
+            <Col className="MainLandingWrapperFooter-nav__col">
+              <h6>Products</h6>
+              <div className="MainLandingWrapperFooter-nav__items">
+                <Link routeName="">Narfex token</Link>
+                <Link routeName="">Exchanger</Link>
+                <Link routeName="">Dex</Link>
+                <Link routeName="">Liqudity</Link>
+                <Link routeName="">Farming</Link>
+                <Link routeName="">Validator</Link>
+              </div>
+            </Col>
+            <Col className="MainLandingWrapperFooter-nav__col">
+              <h6>Company</h6>
+              <Col className="MainLandingWrapperFooter-nav__items">
+                <Link routeName="">Governance</Link>
+                <Link routeName="">Support</Link>
+                <Link routeName="">FAQ</Link>
+              </Col>
+            </Col>
+            <Col className="MainLandingWrapperFooter-nav__col">
+              <h6>Legasy</h6>
+              <Col className="MainLandingWrapperFooter-nav__items">
+                <Link routeName="">Privacy Policy</Link>
+                <Link routeName="">Security</Link>
+              </Col>
+            </Col>
+          </nav>
+          <Col></Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
 
 export default Footer;
