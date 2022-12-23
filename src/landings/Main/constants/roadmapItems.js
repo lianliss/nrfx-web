38613088f56962722502291ae2c082040f35dd00
Arslan-95 <arslan180198@gmail.com>
2022-12-23 16:48:03 +0300

    NRFX-7 Roadmap langs is ready

---
+++ src/landings/Main/constants/roadmapItems.js
@@ -0,0 +1,22 @@
+export const roadmapItems = [
+  {
+    title: 'main_landing_roadmap_1_1',
+    tasks: ['main_landing_roadmap_1_1'],
+  },
+  {
+    title: 'main_landing_roadmap_2_1',
+    tasks: ['main_landing_roadmap_2_1'],
+  },
+  {
+    title: 'main_landing_roadmap_3_1',
+    tasks: ['main_landing_roadmap_3_1'],
+  },
+  {
+    title: 'main_landing_roadmap_4_1',
+    tasks: ['main_landing_roadmap_4_1'],
+  },
+  {
+    title: 'main_landing_roadmap_5_1',
+    tasks: ['main_landing_roadmap_5_1'],
+  },
+];
