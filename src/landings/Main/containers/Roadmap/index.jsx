8e8bc03b41f1b80fb6ee9dbf8ee9292eb2e0954a
Arslan-95 <arslan180198@gmail.com>
2022-12-23 16:47:55 +0300

    NRFX-7 Roadmap langs is ready

--- src/landings/Main/containers/Roadmap/index.jsx
+++ src/landings/Main/containers/Roadmap/index.jsx
@@ -4,8 +4,9 @@ import React from 'react';
 import { default as RoadmapComponent } from 'src/token_landing/containers/Roadmap/Roadmap';
 
 // Utils
-import { roadmapItems } from 'src/token_landing/constants/roadmap';
+import { roadmapItems } from '../../constants/roadmapItems';
 import { types } from 'src/token_landing/containers/Roadmap/constants/roadmapTypes';
+import { getLang } from 'utils';
 
 // Styles
 import './index.less';
@@ -15,7 +16,7 @@ function Roadmap({ adaptive }) {
     <div className="MainLanding-roadmap__wrapper">
       <div className="MainLanding-roadmap">
         <RoadmapComponent
-          title="Road map"
+          title={getLang('main_landing_roadmap_title')}
           items={roadmapItems}
           type={types.medium}
         />
