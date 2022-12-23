2f9b25fcce630560a8127dc7d622601bfc480315
Arslan-95 <arslan180198@gmail.com>
2022-12-23 22:38:47 +0300

    NRFX-7 Footer links is ready

--- src/wrappers/MainLanding/components/Footer/index.jsx
+++ src/wrappers/MainLanding/components/Footer/index.jsx
@@ -9,9 +9,11 @@ import FooterReadDoc from '../FooterReadDoc';
 // Utils
 import socialLinksType from 'src/landings/Main/constants/socialLinksType';
 import { getLang } from 'utils';
+import * as pages from 'src/index/constants/pages';
 
 // Styles
 import './index.less';
+import company from '../../../../index/constants/company';
 
 function Footer({ adaptive }) {
   return (
@@ -26,20 +28,38 @@ function Footer({ adaptive }) {
             <Col className="MainLandingWrapperFooter-nav__col">
               <h6>{getLang('site__footerProducts')}</h6>
               <div className="MainLandingWrapperFooter-nav__items">
-                <Link routeName="">{getLang('main_landing_navbar_token')}</Link>
-                <Link routeName="">{getLang('dapp_sidebar_exchanger')}</Link>
-                <Link routeName="">{getLang('main_landing_footer_dex')}</Link>
-                <Link routeName="">{getLang('dapp_sidebar_liquidity')}</Link>
-                <Link routeName="">{getLang('dapp_farming_page_title')}</Link>
-                <Link routeName="">{getLang('dapp_sidebar_validator')}</Link>
+                <Link routeName={pages.TOKEN_LANDING}>
+                  {getLang('main_landing_navbar_token')}
+                </Link>
+                <Link routeName={pages.DAPP_EXCHANGE}>
+                  {getLang('dapp_sidebar_exchanger')}
+                </Link>
+                <Link routeName={pages.DAPP_SWAP}>
+                  {getLang('main_landing_footer_dex')}
+                </Link>
+                <Link routeName={pages.LIQUIDITY}>
+                  {getLang('dapp_sidebar_liquidity')}
+                </Link>
+                <Link routeName={pages.FARMING}>
+                  {getLang('dapp_farming_page_title')}
+                </Link>
+                <Link routeName={pages.VALIDATOR}>
+                  {getLang('dapp_sidebar_validator')}
+                </Link>
               </div>
             </Col>
             <Col className="MainLandingWrapperFooter-nav__col">
               <h6>{getLang('site__footerCompany')}</h6>
               <Col className="MainLandingWrapperFooter-nav__items">
-                <Link routeName="">{getLang('landing_footer_company')}</Link>
-                <Link routeName="">{getLang('landing_footer_support')}</Link>
-                <Link routeName="">{getLang('landing_footer_faq')}</Link>
+                <Link routeName={pages.NARFEX_DAO}>
+                  {getLang('landing_footer_company')}
+                </Link>
+                <a
+                  href={company.docs + '/narfex/faq/narfex-platform'}
+                  target="_blank"
+                >
+                  {getLang('landing_footer_faq')}
+                </a>
               </Col>
             </Col>
             <Col className="MainLandingWrapperFooter-nav__col">
