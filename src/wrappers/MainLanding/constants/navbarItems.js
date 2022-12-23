f113026f286ed3ad038a17f3a83c1a7c46d7b9cc
Arslan-95 <arslan180198@gmail.com>
2022-12-23 22:31:04 +0300

    NRFX-7 Header is ready

--- src/wrappers/MainLanding/constants/navbarItems.js
+++ src/wrappers/MainLanding/constants/navbarItems.js
@@ -2,6 +2,6 @@ import * as pages from 'src/index/constants/pages';
 
 export default [
   { title: 'main_landing_navbar_dao', route: pages.NARFEX_DAO },
-  { title: 'main_landing_navbar_support', route: pages.MAIN },
+  // { title: 'main_landing_navbar_support', route: pages.MAIN },
   { title: 'main_landing_navbar_token', route: pages.TOKEN_LANDING },
 ];
