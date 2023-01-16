f85660c13bc82d4d6b9fa138d2d732651e0298a7
Arslan-95 <arslan180198@gmail.com>
2023-01-16 11:44:21 +0300

    web-57 langs added to the token landing

--- src/token_landing/constants/roadmap.js
+++ src/token_landing/constants/roadmap.js
@@ -2,25 +2,26 @@ export const roadmapItems = [
   {
     title: 'token_landing_roadmap_1_1',
     tasks: [
-      'token_landing_roadmap_1_1',
       'token_landing_roadmap_1_2',
       'token_landing_roadmap_1_3',
+      'token_landing_roadmap_1_4',
+      'token_landing_roadmap_1_5',
     ],
   },
   {
     title: 'token_landing_roadmap_2_1',
     tasks: [
-      'token_landing_roadmap_2_1',
       'token_landing_roadmap_2_2',
       'token_landing_roadmap_2_3',
       'token_landing_roadmap_2_4',
       'token_landing_roadmap_2_5',
+      'token_landing_roadmap_2_6',
+      'token_landing_roadmap_2_7',
     ],
   },
   {
     title: 'token_landing_roadmap_3_1',
     tasks: [
-      'token_landing_roadmap_3_1',
       'token_landing_roadmap_3_2',
       'token_landing_roadmap_3_3',
       'token_landing_roadmap_3_4',
@@ -29,7 +30,6 @@ export const roadmapItems = [
   {
     title: 'token_landing_roadmap_4_1',
     tasks: [
-      'token_landing_roadmap_4_1',
       'token_landing_roadmap_4_2',
       'token_landing_roadmap_4_3',
       'token_landing_roadmap_4_4',
@@ -40,7 +40,6 @@ export const roadmapItems = [
   {
     title: 'token_landing_roadmap_5_1',
     tasks: [
-      'token_landing_roadmap_5_1',
       'token_landing_roadmap_5_2',
       'token_landing_roadmap_5_3',
       'token_landing_roadmap_5_4',
