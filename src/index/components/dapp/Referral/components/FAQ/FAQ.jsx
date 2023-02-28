a1c95dc199a658632f4f5c0fa972eed729af0f76
Arslan-95 <arslan180198@gmail.com>
2023-02-28 15:04:36 +0300

    NRFX-207 FAQ Component is changed

--- src/index/components/dapp/Referral/components/FAQ/FAQ.jsx
+++ src/index/components/dapp/Referral/components/FAQ/FAQ.jsx
@@ -3,83 +3,19 @@ import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 
 // Components
-import { Row, Col, DropdownElement } from 'src/ui';
-import LineBreaker from 'src/ui/components/LineBreaker/LineBreaker';
-import SVG from 'utils/svg-wrap';
+import { default as DappFAQ } from 'dapp/FAQ';
 
 // Utils
-import { getLang } from 'utils';
-import { currentLangSelector } from 'src/selectors';
+import faq from '../../constants/faq';
 
 // Styles
 import './FAQ.less';
 
 function FAQ({ adaptive, type }) {
-  const lang = useSelector(currentLangSelector);
-  const [items, setItems] = React.useState([]);
-  const leftColumnItems = items.filter((__, i) => i % 2 === 0);
-  const rightColumnItems = items.filter((__, i) => i % 2 !== 0);
-
-  React.useEffect(() => {
-    setItems(getItems());
-  }, [lang]);
-
-  const getItems = () => {
-    const findedQuestions = Array(18)
-      .fill({})
-      .map((__, index) => {
-        const questionLang = `dapp_referral_faq_${type}_${index + 1}_question`;
-        const question = getLang(questionLang);
-        const answerLang = `dapp_referral_faq_${type}_${index + 1}_answer`;
-        const answer = getLang(answerLang);
-
-        if (questionLang === question) {
-          return null;
-        }
-
-        return { question, answer: answer === answerLang ? '' : answer };
-      })
-      .filter((item) => item);
-
-    return findedQuestions;
-  };
-
-  const FAQItem = ({ question, answer }) => {
-    return (
-      <DropdownElement
-        dropElement={
-          <p>
-            <LineBreaker text={answer} />
-          </p>
-        }
-      >
-        <Row justifyContent="space-between" alignItems="center">
-          <p>
-            <LineBreaker text={question} />
-          </p>
-          <SVG
-            src={require('src/asset/icons/arrows/dropdown-small-bordered.svg')}
-          />
-        </Row>
-      </DropdownElement>
-    );
-  };
-
   return (
     <div className="Referral__FAQ">
       <h2>FAQ</h2>
-      <Row wrap={adaptive}>
-        <Col className="Referral__FAQ__items">
-          {leftColumnItems.map((item, key) => (
-            <FAQItem question={item.question} answer={item.answer} key={key} />
-          ))}
-        </Col>
-        <Col className="Referral__FAQ__items">
-          {rightColumnItems.map((item, key) => (
-            <FAQItem question={item.question} answer={item.answer} key={key} />
-          ))}
-        </Col>
-      </Row>
+      <DappFAQ items={faq[type]} adaptive={adaptive} />
     </div>
   );
 }
---
+++ src/index/components/dapp/Referral/constants/faq.js
@@ -0,0 +1,29 @@
+export default {
+  exchanger: [
+    {
+      question: 'dapp_referral_faq_exchanger_1_question',
+      answer: 'dapp_referral_faq_exchanger_1_answer',
+    },
+    {
+      question: 'dapp_referral_faq_exchanger_2_question',
+      answer: 'dapp_referral_faq_exchanger_2_answer',
+    },
+    {
+      question: 'dapp_referral_faq_exchanger_3_question',
+      answer: 'dapp_referral_faq_exchanger_3_answer',
+    },
+    {
+      question: 'dapp_referral_faq_exchanger_4_question',
+      answer: 'dapp_referral_faq_exchanger_4_answer',
+    },
+    {
+      question: 'dapp_referral_faq_exchanger_5_question',
+      answer: 'dapp_referral_faq_exchanger_5_answer',
+    },
+    {
+      question: 'dapp_referral_faq_exchanger_6_question',
+      answer: 'dapp_referral_faq_exchanger_6_answer',
+    },
+  ],
+  farming: [],
+};
