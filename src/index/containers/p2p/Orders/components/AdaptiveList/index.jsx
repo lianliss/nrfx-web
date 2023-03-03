21cb12fd274cb5971332aed14c0b61f4c8f129a4
Arslan-95 <arslan180198@gmail.com>
2023-03-03 11:50:19 +0300

    NRFX-207 Fix

--- src/index/containers/p2p/Orders/components/AdaptiveList/index.jsx
+++ src/index/containers/p2p/Orders/components/AdaptiveList/index.jsx
@@ -17,6 +17,83 @@ import paymentColors from '../../../constants/paymentColors';
 import './index.less';
 
 function AdaptiveList() {
+  const Item = () => (
+    <TR className="orders-list-item">
+      <TD>
+        <div className="orders-list-user">
+          <div className="orders-list-user__profile">
+            <img src={avatar} alt="" className="orders-list-user__avatar" />
+            <span
+              className="orders-list-user__name "
+              title="mail****@gmail.com"
+            >
+              mail****@gmail.com
+            </span>
+            <SVG src={require('src/asset/icons/status/sucess-13px.svg')} flex />
+          </div>
+          <div className="orders-list-user__info">
+            <span>
+              <NumberFormat number={287} /> orders
+            </span>
+            <span>
+              <NumberFormat number={85.7} percent /> completion
+            </span>
+          </div>
+        </div>
+      </TD>
+      <TD>
+        <Row alignItems="center" className="orders-list-price">
+          <WalletIcon currency="IDR" size={24} />
+          <NumberFormat number={15333.33} currency="IDR" />
+        </Row>
+      </TD>
+      <TD>
+        <div className="orders-list-limits">
+          <div className="orders-list-limits__available">
+            <span className="orders-list-limits__title">Available</span>
+            <NumberFormat number={1000.0} currency="USDT" />
+          </div>
+          <div className="orders-list-limits__limit">
+            <span className="orders-list-limits__title">Limit</span>
+            <div>
+              <span className="Number" title={1000000.0}>
+                {`Rp${getFinePrice(1000000.0)}`}
+              </span>
+              <span className="Number">&nbsp;-&nbsp;</span>
+              <span className="Number" title={1030260.0}>
+                {`Rp${getFinePrice(1030260.0)}`}
+              </span>
+            </div>
+          </div>
+        </div>
+      </TD>
+      <TD>
+        <div className="orders-list-buy">
+          <Button type="lightBlue" size="small">
+            Buy
+          </Button>
+        </div>
+      </TD>
+      <TD>
+        <div className="orders-list-payment">
+          <ListPayment
+            title="Bank Transfer"
+            color={paymentColors.orange}
+            adaptive
+          />
+          <ListPayment title="Pay me" color={paymentColors.red} adaptive />
+          <ListPayment title="Mono Bank" color={paymentColors.black} adaptive />
+          <ListPayment
+            title="Bank Transfer"
+            color={paymentColors.orange}
+            adaptive
+          />
+          <ListPayment title="Pay me" color={paymentColors.red} adaptive />
+        </div>
+      </TD>
+    </TR>
+  );
+
   return (
     <CabinetBlock className="orders-list">
       <CabinetTable
@@ -32,87 +109,10 @@ function AdaptiveList() {
           </TR>
         }
       >
-        <TR className="orders-list-item">
-          <TD>
-            <div className="orders-list-user">
-              <div className="orders-list-user__profile">
-                <img src={avatar} alt="" className="orders-list-user__avatar" />
-                <span
-                  className="orders-list-user__name "
-                  title="mail****@gmail.com"
-                >
-                  mail****@gmail.com
-                </span>
-                <SVG
-                  src={require('src/asset/icons/status/sucess-13px.svg')}
-                  flex
-                />
-              </div>
-              <div className="orders-list-user__info">
-                <span>
-                  <NumberFormat number={287} /> orders
-                </span>
-                <span>
-                  <NumberFormat number={85.7} percent /> completion
-                </span>
-              </div>
-            </div>
-          </TD>
-          <TD>
-            <Row alignItems="center" className="orders-list-price">
-              <WalletIcon currency="IDR" size={24} />
-              <NumberFormat number={15333.33} currency="IDR" />
-            </Row>
-          </TD>
-          <TD>
-            <div className="orders-list-limits">
-              <div className="orders-list-limits__available">
-                <span className="orders-list-limits__title">Available</span>
-                <NumberFormat number={1000.0} currency="USDT" />
-              </div>
-              <div className="orders-list-limits__limit">
-                <span className="orders-list-limits__title">Limit</span>
-                <div>
-                  <span className="Number" title={1000000.0}>
-                    {`Rp${getFinePrice(1000000.0)}`}
-                  </span>
-                  <span className="Number">&nbsp;-&nbsp;</span>
-                  <span className="Number" title={1030260.0}>
-                    {`Rp${getFinePrice(1030260.0)}`}
-                  </span>
-                </div>
-              </div>
-            </div>
-          </TD>
-          <TD>
-            <div className="orders-list-buy">
-              <Button type="lightBlue" size="small">
-                Buy
-              </Button>
-            </div>
-          </TD>
-          <TD>
-            <div className="orders-list-payment">
-              <ListPayment
-                title="Bank Transfer"
-                color={paymentColors.orange}
-                adaptive
-              />
-              <ListPayment title="Pay me" color={paymentColors.red} adaptive />
-              <ListPayment
-                title="Mono Bank"
-                color={paymentColors.black}
-                adaptive
-              />
-              <ListPayment
-                title="Bank Transfer"
-                color={paymentColors.orange}
-                adaptive
-              />
-              <ListPayment title="Pay me" color={paymentColors.red} adaptive />
-            </div>
-          </TD>
-        </TR>
+        <Item />
+        <Item />
+        <Item />
+        <Item />
       </CabinetTable>
     </CabinetBlock>
   );
--- src/index/containers/p2p/Orders/components/List/index.jsx
+++ src/index/containers/p2p/Orders/components/List/index.jsx
@@ -17,6 +17,73 @@ import paymentColors from '../../../constants/paymentColors';
 import './index.less';
 
 function List() {
+  const Item = () => (
+    <TR className="orders-list-item">
+      <TD>
+        <div className="orders-list-user">
+          <div className="orders-list-user__profile">
+            <img src={avatar} alt="" className="orders-list-user__avatar" />
+            <span
+              className="orders-list-user__name "
+              title="mail****@gmail.com"
+            >
+              mail****@gmail.com
+            </span>
+            <SVG src={require('src/asset/icons/status/sucess-13px.svg')} flex />
+          </div>
+          <div className="orders-list-user__info">
+            <span>
+              <NumberFormat number={287} /> orders
+            </span>
+            <span>
+              <NumberFormat number={85.7} percent /> completion
+            </span>
+          </div>
+        </div>
+      </TD>
+      <TD>
+        <Row alignItems="center" className="orders-list-price">
+          <WalletIcon currency="IDR" size={24} />
+          <NumberFormat number={15333.33} currency="IDR" />
+        </Row>
+      </TD>
+      <TD>
+        <div className="orders-list-limits">
+          <div className="orders-list-limits__available">
+            <span className="orders-list-limits__title">Available</span>
+            <NumberFormat number={1000.0} currency="USDT" />
+          </div>
+          <div className="orders-list-limits__limit">
+            <span className="orders-list-limits__title">Limit</span>
+            <div>
+              <span className="Number" title={1000000.0}>
+                {`Rp${getFinePrice(1000000.0)}`}
+              </span>
+              <span className="Number">&nbsp;-&nbsp;</span>
+              <span className="Number" title={1030260.0}>
+                {`Rp${getFinePrice(1030260.0)}`}
+              </span>
+            </div>
+          </div>
+        </div>
+      </TD>
+      <TD>
+        <div className="orders-list-payment">
+          <ListPayment title="Bank Transfer" color={paymentColors.orange} />
+          <ListPayment title="Pay me" color={paymentColors.red} />
+          <ListPayment title="Mono Bank" color={paymentColors.black} />
+          <ListPayment title="Bank Transfer" color={paymentColors.orange} />
+          <ListPayment title="Pay me" color={paymentColors.red} />
+        </div>
+      </TD>
+      <TD>
+        <Button type="lightBlue" size="small">
+          Buy N-fiat
+        </Button>
+      </TD>
+    </TR>
+  );
+
   return (
     <CabinetBlock className="orders-list">
       <CabinetTable
@@ -32,73 +99,10 @@ function List() {
           </TR>
         }
       >
-        <TR className="orders-list-item">
-          <TD>
-            <div className="orders-list-user">
-              <div className="orders-list-user__profile">
-                <img src={avatar} alt="" className="orders-list-user__avatar" />
-                <span
-                  className="orders-list-user__name "
-                  title="mail****@gmail.com"
-                >
-                  mail****@gmail.com
-                </span>
-                <SVG
-                  src={require('src/asset/icons/status/sucess-13px.svg')}
-                  flex
-                />
-              </div>
-              <div className="orders-list-user__info">
-                <span>
-                  <NumberFormat number={287} /> orders
-                </span>
-                <span>
-                  <NumberFormat number={85.7} percent /> completion
-                </span>
-              </div>
-            </div>
-          </TD>
-          <TD>
-            <Row alignItems="center" className="orders-list-price">
-              <WalletIcon currency="IDR" size={24} />
-              <NumberFormat number={15333.33} currency="IDR" />
-            </Row>
-          </TD>
-          <TD>
-            <div className="orders-list-limits">
-              <div className="orders-list-limits__available">
-                <span className="orders-list-limits__title">Available</span>
-                <NumberFormat number={1000.0} currency="USDT" />
-              </div>
-              <div className="orders-list-limits__limit">
-                <span className="orders-list-limits__title">Limit</span>
-                <div>
-                  <span className="Number" title={1000000.0}>
-                    {`Rp${getFinePrice(1000000.0)}`}
-                  </span>
-                  <span className="Number">&nbsp;-&nbsp;</span>
-                  <span className="Number" title={1030260.0}>
-                    {`Rp${getFinePrice(1030260.0)}`}
-                  </span>
-                </div>
-              </div>
-            </div>
-          </TD>
-          <TD>
-            <div className="orders-list-payment">
-              <ListPayment title="Bank Transfer" color={paymentColors.orange} />
-              <ListPayment title="Pay me" color={paymentColors.red} />
-              <ListPayment title="Mono Bank" color={paymentColors.black} />
-              <ListPayment title="Bank Transfer" color={paymentColors.orange} />
-              <ListPayment title="Pay me" color={paymentColors.red} />
-            </div>
-          </TD>
-          <TD>
-            <Button type="lightBlue" size="small">
-              Buy N-fiat
-            </Button>
-          </TD>
-        </TR>
+        <Item />
+        <Item />
+        <Item />
+        <Item />
       </CabinetTable>
     </CabinetBlock>
   );
