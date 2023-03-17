7afe0c81f1b3de8e3928bc4b32b6dbb9985a12ba
Arslan-95 <arslan180198@gmail.com>
2023-03-17 16:29:13 +0300

    web-60 fix form

--- src/landings/TokenMigration/components/Form/index.jsx
+++ src/landings/TokenMigration/components/Form/index.jsx
@@ -5,24 +5,27 @@ import { Button, NumberFormat, Row } from 'ui';
 import SVG from 'utils/svg-wrap';
 
 // Utils
+import { classNames as cn } from 'utils';
 import successIcon from 'src/asset/icons/status/check_circle_success.svg';
 
 // Styles
 import './index.less';
 
-function Form({ isConnected = true, approved = true }) {
+function Form({ isConnected = false, approved = true }) {
   return (
-    <div className="TokenMigrationLanding-form">
-      <h3>You exchange NRFX to NRFX V2</h3>
-      <p className="TokenMigrationLanding-form__balance">
-        <NumberFormat number={0.0} currency="NRFX" />
-      </p>
-      <p className="TokenMigrationLanding-form__description">
-        If you experienced an error or believe your estimate is incorrect,
-        please email us at support@narfex.com. Provide your wallet address, a
-        clear explanation of your issue, and any relevant blockchain
-        transactions. We will respond within 5 business days.
-      </p>
+    <div className={cn('TokenMigrationLanding-form', { isConnected })}>
+      <div className="TokenMigrationLanding-form__content">
+        <h3>You exchange NRFX to NRFX V2</h3>
+        <p className="TokenMigrationLanding-form__balance">
+          <NumberFormat number={0.0} currency="NRFX" />
+        </p>
+        <p className="TokenMigrationLanding-form__description">
+          If you experienced an error or believe your estimate is incorrect,
+          please email us at support@narfex.com. Provide your wallet address, a
+          clear explanation of your issue, and any relevant blockchain
+          transactions. We will respond within 5 business days.
+        </p>
+      </div>
       <div className="TokenMigrationLanding-form__buttons">
         {isConnected ? (
           <>
