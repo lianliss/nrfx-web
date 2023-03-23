af2eaf44c48666d3916a3e345c83323501bb2e33
Arslan-95 <arslan180198@gmail.com>
2023-03-23 19:14:29 +0300

    NRFX-246 Order process is created

---
+++ src/index/containers/p2p/Order/components/Process/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Components
+import { CabinetBlock } from 'dapp';
+
+// Styles
+import './index.less';
+
+function Process() {
+  return <CabinetBlock className="p2p-order-process"></CabinetBlock>;
+}
+
+export default Process;
