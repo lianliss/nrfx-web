fe44714090f1e923168b65dec93fd45d4d6dfcfe
Arslan-95 <arslan180198@gmail.com>
2023-03-24 08:54:39 +0300

    NRFX-246 Titles for the method is ready

--- src/index/containers/p2p/Order/components/Process/index.jsx
+++ src/index/containers/p2p/Order/components/Process/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 // Components
-import { Row, Col, NumberFormat } from 'ui';
+import { Row, Col, NumberFormat, HoverPopup } from 'ui';
 import { CabinetBlock } from 'dapp';
+import SVG from 'utils/svg-wrap';
 
 // Utils
 import { classNames as cn } from 'utils';
@@ -53,6 +54,24 @@ function Process() {
             <Info title="Amount" prefix="Rp" number={600812255} />
             <Info title="Price" prefix="Rp" number={15555000} />
             <Info title="Quantity" currency="USDT" number={389.88} />
+          </Row>
+        </div>
+        <div className="p2p-order-process-method">
+          <Row alignItems="center">
+            <h5>Transfer the funds to the sellers account provided below </h5>
+            <HoverPopup content="Answer" size="small">
+              <SVG
+                src={require('src/asset/icons/cabinet/question-icon.svg')}
+                className="question-icon"
+                flex
+                width="14"
+                height="14"
+              />
+            </HoverPopup>
+          </Row>
+          <Row className="p2p-order-process-method__warn" alignItems="center">
+            <SVG src={require('src/asset/icons/status/warn-orange.svg')} />
+            <p>Binance only supports real-name verified payment methods.</p>
           </Row>
         </div>
       </div>
