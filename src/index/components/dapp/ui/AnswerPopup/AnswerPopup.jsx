3f36c283d89118f602f18dff8b97c17e0a702629
Arslan-95 <arslan180198@gmail.com>
2023-03-28 16:36:59 +0300

    NRFX-246 AnswerPopup is updated

--- src/index/components/dapp/ui/AnswerPopup/AnswerPopup.jsx
+++ src/index/components/dapp/ui/AnswerPopup/AnswerPopup.jsx
@@ -6,12 +6,16 @@ import SVG from 'utils/svg-wrap';
 // Styles
 import './AnswerPopup.less';
 
-function AnswerPopup({ children }) {
+function AnswerPopup({ question, children }) {
   return (
     <div className="AnswerPopup">
-      <div className="AnswerPopup__icon">
-        <SVG src={require('src/asset/icons/cabinet/question-icon.svg')} />
-      </div>
+      {question ? (
+        <div className="AnswerPopup__question">{question}</div>
+      ) : (
+        <div className="AnswerPopup__icon">
+          <SVG src={require('src/asset/icons/cabinet/question-icon.svg')} />
+        </div>
+      )}
       <div className="AnswerPopup__container">
         <div className="AnswerPopup__triangle">
           <SVG src={require('./assets/triangle.svg')} />
