07a417128ebed801ce60b5ee9db6ee03f5ad056e
Arslan-95 <arslan180198@gmail.com>
2023-03-31 19:21:29 +0300

    NRFX-247 Completed method is ready

--- src/index/containers/p2p/Order/components/ChoosedMethod/index.jsx
+++ src/index/containers/p2p/Order/components/ChoosedMethod/index.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import PaymentItem from 'src/index/components/p2p/components/PaymentItem';
 import paymentColors from '../../../constants/paymentColors';
 
+// Styles
+import './index.less';
+
 function ChoosedMethod({ payment }) {
   return (
     <div className="p2p-order-choosed-method">
