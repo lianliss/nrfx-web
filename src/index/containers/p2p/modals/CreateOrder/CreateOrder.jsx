b7dcea254c862fecc7efb6631b44f8453baa8d2a
Arslan-95 <arslan180198@gmail.com>
2023-04-03 17:56:00 +0300

    dapp-p2p Buy n-fiat modal form is ready

---
+++ src/index/containers/p2p/modals/CreateOrder/CreateOrder.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+// Components
+import { CabinetModal, DappInput, CustomButton } from 'dapp';
+import { Row, Button, NumberFormat } from 'ui';
+
+// Utils
+import { adaptiveSelector } from 'src/selectors';
+
+// Styles
+import './CreateOrder.less';
+
+const CurrencyIndicator = ({ currency }) => (
+  <span className="moderate-fz medium-fw dark-black-color">{currency}</span>
+);
+
+const Label = ({ text }) => (
+  <p className="cool-gray-color modal__label">{text}</p>
+);
+
+function CreateOrder(props) {
+  const adaptive = useSelector(adaptiveSelector);
+
+  return (
+    <CabinetModal className="p2p-modal-create-order__wrapper">
+      <Row className="p2p-modal-create-order" wrap={adaptive}>
+        <div></div>
+        <div className="p2p-modal-create-order-form">
+          <div>
+            <Label text="I want to pay" />
+            <DappInput
+              placeholder="0.00"
+              inputMode="decimal"
+              indicator={
+                <Row alignItems="center" gap={8}>
+                  <CustomButton>
+                    <span className="light-blue-gradient-color">ALL</span>
+                  </CustomButton>
+                  <CurrencyIndicator currency="IDR" />
+                </Row>
+              }
+              className="moderate-fz medium-fw"
+            />
+            {!adaptive && (
+              <p className="cool-gray-color moderate-fz normal-fw modal-balance">
+                Balance: <NumberFormat number={0.0} currency="USDT" />
+              </p>
+            )}
+          </div>
+          <div>
+            <Label text="I will receive" />
+            <DappInput
+              placeholder="1,000.00 - 8,999.60"
+              indicator={<CurrencyIndicator currency="IDR" />}
+              className="moderate-fz medium-fw"
+              inputMode="decimal"
+              type="number"
+            />
+          </div>
+          <Row className="modal-buttons" gap="15px 6px">
+            {!adaptive && (
+              <Button type="secondary-light" size="moderate">
+                <span className="light-blue-gradient-color">Cancel</span>
+              </Button>
+            )}
+            <Button type="lightBlue" size={adaptive ? 'big' : 'moderate'}>
+              Buy USDT
+            </Button>
+          </Row>
+        </div>
+      </Row>
+    </CabinetModal>
+  );
+}
+
+export default CreateOrder;
