ae4d1aa857112dbcd27af28a66d2c1047785dd3c
Arslan-95 <arslan180198@gmail.com>
2023-04-14 00:23:31 +0300

    [validator] AdminPanel - Switch block is ready

--- src/index/containers/validator/AdminPanel/containers/PaymentMethods/PaymentMethods.jsx
+++ src/index/containers/validator/AdminPanel/containers/PaymentMethods/PaymentMethods.jsx
@@ -1,29 +1,51 @@
 import React from 'react';
 
 // Components
-import { Button } from 'ui';
+import { Button, SwitchTabs } from 'ui';
 
 // Styles
 import styles from './PaymentMethods.module.less';
 
 function PaymentMethods({ adaptive }) {
-  return (
-    <div className={styles.PaymentMethods}>
-      <h2 className={styles.PaymentMethods__title}>P2P Payment Methods</h2>
-      <div className={styles.PaymentMethods__subtitle__wrapper}>
-        <p className={styles.PaymentMethods__subtitle}>
-          P2P payment methods: When you sell cryptocurrencies, the payment
-          method added will be displayed to buyer as options to accept payment,
-          please ensure that the account owner’s name is consistent with your
-          verified name on Binance. You can add up to 20 payment methods.
-        </p>
-        <Button type="secondary-light" size={adaptive ? 'big' : 'moderate'}>
-          <span className="light-blue-gradient-color">
-            Add a payment method
-          </span>
-        </Button>
-      </div>
+  const [selected, setSelected] = React.useState('methods');
+
+  const Switch = () => (
+    <div className={styles.switchContainer}>
+      <SwitchTabs
+        selected={selected}
+        onChange={setSelected}
+        type="secondary-alice"
+        tabs={[
+          { value: 'methods', label: 'P2P Payment Methods' },
+          { value: 'feedback', label: 'Feedback (0)' },
+          { value: 'blacklist', label: 'Blacklist' },
+        ]}
+      />
     </div>
+  );
+
+  return (
+    <>
+      {adaptive && <Switch />}
+      <div className={styles.PaymentMethods}>
+        {!adaptive && <Switch />}
+        <h2 className={styles.PaymentMethods__title}>P2P Payment Methods</h2>
+        <div className={styles.PaymentMethods__subtitle__wrapper}>
+          <p className={styles.PaymentMethods__subtitle}>
+            P2P payment methods: When you sell cryptocurrencies, the payment
+            method added will be displayed to buyer as options to accept
+            payment, please ensure that the account owner’s name is consistent
+            with your verified name on Binance. You can add up to 20 payment
+            methods.
+          </p>
+          <Button type="secondary-light" size={adaptive ? 'big' : 'moderate'}>
+            <span className="light-blue-gradient-color">
+              Add a payment method
+            </span>
+          </Button>
+        </div>
+      </div>
+    </>
   );
 }
 
