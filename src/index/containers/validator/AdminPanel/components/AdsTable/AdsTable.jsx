db7b655d7f3128c6ce21af33df7573d42e52d689
Arslan-95 <arslan180198@gmail.com>
2023-04-19 02:04:14 +0300

    [validator] Adaptive table is ready

--- src/index/containers/validator/AdminPanel/components/AdsTable/AdsTable.jsx
+++ src/index/containers/validator/AdminPanel/components/AdsTable/AdsTable.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import CabinetTable, { TR, TD } from 'dapp/CabinetTable/CabinetTable';
 import { CustomButton } from 'dapp';
@@ -7,6 +8,8 @@ import SVG from 'utils/svg-wrap';
 
 import styles from './AdsTable.module.less';
 import PaymentItem from 'src/index/components/p2p/components/PaymentItem';
+import testItems from './testItems';
+import { classNames as cn } from 'utils';
 
 const IconButton = ({ icon, onClick }) => (
   <CustomButton className={styles.AdsTable__action__button} onClick={onClick}>
@@ -14,7 +17,166 @@ const IconButton = ({ icon, onClick }) => (
   </CustomButton>
 );
 
-const AdsTable = (props) => {
+const AdaptiveTD = ({ title, children, value, className }) => (
+  <TD className={styles.adaptiveTD}>
+    <Row justifyContent="space-between">
+      <span className={styles.adaptiveTD__title}>{title}</span>
+      {typeof value !== undefined && (
+        <span className={cn(styles.adaptiveTD__value, className)}>{value}</span>
+      )}
+      {children}
+    </Row>
+  </TD>
+);
+
+const TableRow = ({
+  adaptive,
+  asset,
+  fiat,
+  qty,
+  availableAmount,
+  orderLimit,
+  orderType,
+  adNumber,
+  price,
+  exchangeRate,
+  paymentMethods = [],
+  createTime,
+  lastUpdated,
+  status,
+}) => {
+  const orderLimitNumbers = `${orderLimit.start}-${orderLimit.end}`;
+
+  const actionButtons = (
+    <Row gap={12} className={styles.AdsTable__action__buttons}>
+      <IconButton icon={require('src/asset/icons/action/download.svg')} />
+      <IconButton icon={require('src/asset/icons/action/edit-pencil.svg')} />
+      <IconButton icon={require('src/asset/icons/action/copy-plus.svg')} />
+      <IconButton icon={require('src/asset/icons/action/close-circle.svg')} />
+    </Row>
+  );
+
+  if (adaptive) {
+    return (
+      <TR className={styles.adaptiveTR}>
+        <TD>
+          <Row gap={16}>
+            <span className="green-color">{orderType.toUpperCase()}</span>
+            <span className={styles.cutText}>{adNumber}</span>
+          </Row>
+        </TD>
+        <AdaptiveTD title="Asset/Fiat" value={`${asset}/${fiat}`} />
+        <AdaptiveTD title="Completed Trade QTY." value={qty} />
+        <AdaptiveTD title="Available Amount" value={availableAmount} />
+        <AdaptiveTD title="Price" value={price} />
+        <AdaptiveTD title="Exchange Rate" value={exchangeRate} />
+        <AdaptiveTD title="Order Limit" value={orderLimitNumbers} />
+        <AdaptiveTD title="Payment Method">
+          <Col gap={8} alignItems="flex-end">
+            {paymentMethods.map((item, key) => (
+              <PaymentItem
+                title={item.title}
+                color={null}
+                className={styles.paymentItem}
+                key={key}
+              />
+            ))}
+          </Col>
+        </AdaptiveTD>
+        <AdaptiveTD title="Status" value={status} className="green-color" />
+        <AdaptiveTD
+          title="Create Time:"
+          value={`${createTime.date} ${createTime.time}`}
+        />
+        <AdaptiveTD
+          title="Last Updated"
+          value={`${lastUpdated.date} ${lastUpdated.time}`}
+        />
+        <AdaptiveTD title="Actions">{actionButtons}</AdaptiveTD>
+      </TR>
+    );
+  }
+
+  return (
+    <TR>
+      <TD>
+        <Col>
+          <span className={styles.cutText}>{adNumber}</span>
+          <span className="green-color">{orderType.toUpperCase()}</span>
+          <span>
+            {asset} / {fiat}
+          </span>
+        </Col>
+      </TD>
+      <TD>
+        <Col>
+          <span>{availableAmount}</span>
+          <span>{qty}</span>
+          <span>{orderLimitNumbers} UAH</span>
+        </Col>
+      </TD>
+      <TD>
+        <Col>
+          <span>{price}</span>
+          <span>{exchangeRate}</span>
+        </Col>
+      </TD>
+      <TD>
+        <Col>
+          {paymentMethods.map((paymentMethod, key) => (
+            <PaymentItem title={paymentMethod.title} key={key} />
+          ))}
+        </Col>
+      </TD>
+      <TD>
+        <Col>
+          <Col>
+            <span>{createTime.date}</span>
+            <span>{createTime.time}</span>
+          </Col>
+          <Col>
+            <span>{lastUpdated.date}</span>
+            <span>{lastUpdated.time}</span>
+          </Col>
+        </Col>
+      </TD>
+      <TD className={styles.TD__published}>
+        <span className="green-color">{status}</span>
+      </TD>
+      <TD>{actionButtons}</TD>
+    </TR>
+  );
+};
+
+TableRow.propTypes = {
+  adaptive: PropTypes.bool,
+  asset: PropTypes.string,
+  fiat: PropTypes.string,
+  qty: PropTypes.number,
+  availableAmount: PropTypes.number,
+  orderLimit: PropTypes.shape({
+    start: PropTypes.number,
+    end: PropTypes.number,
+  }),
+  orderType: PropTypes.oneOf(['buy', 'sell']),
+  adNumber: PropTypes.number,
+  price: PropTypes.number,
+  exchangeRate: PropTypes.string,
+  paymentMethods: PropTypes.array,
+  // Can be changed to Date type.
+  createTime: PropTypes.shape({
+    date: PropTypes.string,
+    time: PropTypes.string,
+  }),
+  lastUpdated: PropTypes.shape({
+    date: PropTypes.string,
+    time: PropTypes.string,
+  }),
+  // ---
+  status: PropTypes.string,
+};
+
+const AdsTable = ({ adaptive }) => {
   return (
     <div className={styles.AdsTable}>
       <CabinetTable
@@ -31,66 +193,9 @@ const AdsTable = (props) => {
         }
         type="column"
       >
-        <TR>
-          <TD>
-            <Col>
-              <span className={styles.cutText}>24868475625292182139</span>
-              <span className="green-color">BUY</span>
-              <span>USDT / UAH</span>
-            </Col>
-          </TD>
-          <TD>
-            <Col>
-              <span>100.00</span>
-              <span>0.00</span>
-              <span>500.00-600.00 UAH</span>
-            </Col>
-          </TD>
-          <TD>
-            <Col>
-              <span>30.92</span>
-              <span>- -</span>
-            </Col>
-          </TD>
-          <TD>
-            <Col>
-              <PaymentItem title="Privat Bank" />
-              <PaymentItem title="Privat Monobank bank text" />
-              <PaymentItem title="Privat Bank" />
-            </Col>
-          </TD>
-          <TD>
-            <Col>
-              <Col>
-                <span>2023-04-04</span>
-                <span>05:03:59</span>
-              </Col>
-              <Col>
-                <span>2023-04-04</span>
-                <span>05:03:59</span>
-              </Col>
-            </Col>
-          </TD>
-          <TD className={styles.TD__published}>
-            <span className="green-color">Published</span>
-          </TD>
-          <TD>
-            <Row gap={12}>
-              <IconButton
-                icon={require('src/asset/icons/action/download.svg')}
-              />
-              <IconButton
-                icon={require('src/asset/icons/action/edit-pencil.svg')}
-              />
-              <IconButton
-                icon={require('src/asset/icons/action/copy-plus.svg')}
-              />
-              <IconButton
-                icon={require('src/asset/icons/action/close-circle.svg')}
-              />
-            </Row>
-          </TD>
-        </TR>
+        {testItems.map((item) => (
+          <TableRow {...item} key={item.adNumber} adaptive={adaptive} />
+        ))}
       </CabinetTable>
     </div>
   );
